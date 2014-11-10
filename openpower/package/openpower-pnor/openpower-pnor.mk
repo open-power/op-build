4ee9c5e7bbff55d7a212f3caf10ed9bf97558b7f
Brian Silver <bsilver@us.ibm.com>
2014-11-10 08:11:22 -0600

    Palmetto Live VPD

--- openpower/package/openpower-pnor/openpower-pnor.mk
+++ openpower/package/openpower-pnor/openpower-pnor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_VERSION = 0855f1b9683e13583248a64454fe13fb02fd1b87
+OPENPOWER_PNOR_VERSION = 83c3c2f5ac841b440b96eff316d99ba1d1969403
 OPENPOWER_PNOR_SITE = $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 OPENPOWER_PNOR_LICENSE = Apache-2.0
 OPENPOWER_PNOR_DEPENDENCIES = hostboot hostboot-binaries openpower-targeting skiboot host-openpower-ffs
@@ -12,11 +12,6 @@ OPENPOWER_PNOR_DEPENDENCIES = hostboot hostboot-binaries openpower-targeting ski
 OPENPOWER_PNOR_INSTALL_IMAGES = YES
 OPENPOWER_PNOR_INSTALL_TARGET = NO
 
-#OPENPOWER_PNOR_FILENAME = palmetto.pnor
-#OPENPOWER_PNOR_TARGETING_FILE = $(STAGING_DIR)/openpower_targeting/PALMETTO_HB_targeting.bin
-
-
-#ECC_TOOL_DIR ???
 OPENPOWER_TARGETING_DIR=$(STAGING_DIR)/openpower_targeting/
 HOSTBOOT_IMAGE_DIR=$(STAGING_DIR)/hostboot_build_images/
 HOSTBOOT_BINARY_DIR = $(STAGING_DIR)/hostboot_binaries/
@@ -24,12 +19,27 @@ OPENPOWER_PNOR_SCRATCH_DIR = $(STAGING_DIR)/openpower_pnor_scratch/
 
 define OPENPOWER_PNOR_INSTALL_IMAGES_CMDS
         mkdir -p $(OPENPOWER_PNOR_SCRATCH_DIR)
-        $(TARGET_MAKE_ENV) $(@D)/update_image_$(BR2_OPENPOWER_CONFIG_NAME).pl -op_target_dir $(STAGING_DIR)/openpower_targeting/ -hb_image_dir $(HOSTBOOT_IMAGE_DIR) -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) -hb_binary_dir $(HOSTBOOT_BINARY_DIR) -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME) -targeting_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME)
+        $(TARGET_MAKE_ENV) $(@D)/update_image_$(BR2_OPENPOWER_CONFIG_NAME).pl \
+            -op_target_dir $(STAGING_DIR)/openpower_targeting/ \
+            -hb_image_dir $(HOSTBOOT_IMAGE_DIR) \
+            -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
+            -hb_binary_dir $(HOSTBOOT_BINARY_DIR) \
+            -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME) \
+            -targeting_binary_source $(BR2_OPENPOWER_TARGETING_BIN_FILENAME)
 
         mkdir -p $(STAGING_DIR)/pnor/
-        $(TARGET_MAKE_ENV) $(@D)/create_pnor_image.pl -xml_layout_file $(@D)/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) -pnor_filename $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) -hb_image_dir $(HOSTBOOT_IMAGE_DIR) -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) -outdir $(STAGING_DIR)/pnor/ -payload $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_NAME) -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME)
+        $(TARGET_MAKE_ENV) $(@D)/create_pnor_image.pl \
+            -xml_layout_file $(@D)/$(BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME) \
+            -pnor_filename $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) \
+            -hb_image_dir $(HOSTBOOT_IMAGE_DIR) \
+            -scratch_dir $(OPENPOWER_PNOR_SCRATCH_DIR) \
+            -outdir $(STAGING_DIR)/pnor/ \
+            -payload $(BINARIES_DIR)/$(BR2_SKIBOOT_LID_NAME) \
+            -sbe_binary_filename $(BR2_HOSTBOOT_BINARY_SBE_FILENAME) \
+            -sbec_binary_filename $(BR2_HOSTBOOT_BINARY_SBEC_FILENAME) \
+            -targeting_binary_filename $(BR2_OPENPOWER_TARGETING_ECC_FILENAME)
 
-	$(INSTALL) $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(BINARIES_DIR)
+        $(INSTALL) $(STAGING_DIR)/pnor/$(BR2_OPENPOWER_PNOR_FILENAME) $(BINARIES_DIR)
 endef
 
 $(eval $(generic-package))
--- openpower/package/openpower-targeting/openpower-targeting.mk
+++ openpower/package/openpower-targeting/openpower-targeting.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_TARGETING_VERSION = adda3365283b7bcfcd1f19373b01acfc7669a84f
+OPENPOWER_TARGETING_VERSION = 8fcf5ac8a0d3b1773323c92bef0209424f152434
 OPENPOWER_TARGETING_SITE = $(call github,open-power,hostboot-targeting,$(OPENPOWER_TARGETING_VERSION))
 OPENPOWER_TARGETING_LICENSE = Apache-2.0
 OPENPOWER_TARGETING_DEPENDENCIES = hostboot-install-images
@@ -15,9 +15,21 @@ OPENPOWER_TARGETING_INSTALL_TARGET = NO
 define OPENPOWER_TARGETING_INSTALL_IMAGES_CMDS
         mkdir -p $(STAGING_DIR)/openpower_targeting/;
 
-        $(STAGING_DIR)/hostboot_build_images/mergexml.sh $(@D)/$(BR2_OPENPOWER_TARGETING_SYSTEM_XML_FILENAME) $(STAGING_DIR)/hostboot_build_images/attribute_types.xml $(STAGING_DIR)/hostboot_build_images/attribute_types_hb.xml $(STAGING_DIR)/hostboot_build_images/target_types_merged.xml $(STAGING_DIR)/hostboot_build_images/target_types_hb.xml $(@D)/$(BR2_OPENPOWER_TARGETING_MRW_XML_FILENAME) > $(STAGING_DIR)/openpower_targeting/temporary_hb.hb.xml;
+        $(STAGING_DIR)/hostboot_build_images/mergexml.sh \
+            $(@D)/$(BR2_OPENPOWER_TARGETING_SYSTEM_XML_FILENAME) \
+            $(STAGING_DIR)/hostboot_build_images/attribute_types.xml \
+            $(STAGING_DIR)/hostboot_build_images/attribute_types_hb.xml \
+            $(STAGING_DIR)/hostboot_build_images/target_types_merged.xml \
+            $(STAGING_DIR)/hostboot_build_images/target_types_hb.xml \
+            $(@D)/$(BR2_OPENPOWER_TARGETING_MRW_XML_FILENAME) > $(STAGING_DIR)/openpower_targeting/temporary_hb.hb.xml;
 
-        $(STAGING_DIR)/hostboot_build_images/xmltohb.pl  --hb-xml-file=$(STAGING_DIR)/openpower_targeting/temporary_hb.hb.xml --fapi-attributes-xml-file=$(STAGING_DIR)/hostboot_build_images/fapiattrs.xml --src-output-dir=none --img-output-dir=$(STAGING_DIR)/openpower_targeting/ --vmm-consts-file=$(STAGING_DIR)/hostboot_build_images/vmmconst.h --noshort-enums
+        $(STAGING_DIR)/hostboot_build_images/xmltohb.pl \
+            --hb-xml-file=$(STAGING_DIR)/openpower_targeting/temporary_hb.hb.xml \
+            --fapi-attributes-xml-file=$(STAGING_DIR)/hostboot_build_images/fapiattrs.xml \
+            --src-output-dir=none \
+            --img-output-dir=$(STAGING_DIR)/openpower_targeting/ \
+            --vmm-consts-file=$(STAGING_DIR)/hostboot_build_images/vmmconst.h \
+            --noshort-enums
 
         mv $(STAGING_DIR)/openpower_targeting/targeting.bin $(STAGING_DIR)/openpower_targeting/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME)
 endef
