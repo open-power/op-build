cb56555e58acc6b08ada42ac828792d079b3273c
Brian Silver <bsilver@us.ibm.com>
2014-12-11 12:51:36 -0600

    Add OCC buildroot tooling

---
+++ openpower/package/occ/occ.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# occ
+#
+################################################################################
+
+OCC_VERSION ?= 6d6e33e87de7f4413f8a6c4712059d439159fded
+OCC_SITE ?= $(call github,open-power,occ,$(OCC_VERSION))
+OCC_LICENSE = Apache-2.0
+OCC_DEPENDENCIES = host-binutils p8-pore-binutils
+
+OCC_INSTALL_IMAGES = YES
+OCC_INSTALL_TARGET = NO
+
+OCC_STAGING_DIR = $(STAGING_DIR)/occ
+
+define OCC_BUILD_CMDS
+        cd $(@D)/src && \
+        make POREPATH=$(P8_PORE_BINUTILS_BIN)/bin/ OCC_OP_BUILD=1 CROSS_PREFIX=$(TARGET_CROSS) all && \
+        make combineImage
+endef
+
+define OCC_INSTALL_IMAGES_CMDS
+       mkdir -p $(STAGING_DIR)/occ
+       cp $(@D)/src/image.bin $(OCC_STAGING_DIR)/$(BR2_OCC_BIN_FILENAME)
+endef
+
+$(eval $(generic-package))
