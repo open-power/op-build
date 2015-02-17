ec978915758df0dd8a3cc78bda204971b0fd4c40
Patrick Williams <iawillia@us.ibm.com>
2015-02-17 09:09:32 -0600

    p8-pore-binutils: update to 1d6a41

--- openpower/package/p8-pore-binutils/p8-pore-binutils.mk
+++ openpower/package/p8-pore-binutils/p8-pore-binutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-P8_PORE_BINUTILS_VERSION ?= dcf4f87c0d9e8c8092ace0eb696189ee1056da30
+P8_PORE_BINUTILS_VERSION ?= 1d6a41d5a5104ce9bc8b6ad59f8e58c3a50824a4
 P8_PORE_BINUTILS_SITE ?= $(call github,open-power,p8-pore-binutils,$(P8_PORE_BINUTILS_VERSION))
 P8_PORE_BINUTILS_LICENSE = GPLv3+
 
