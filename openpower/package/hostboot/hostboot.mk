c755e1c9ca7609c801260e3ab1794648cc85e1c1
Jay Azurin <jmazurin@us.ibm.com>
2015-09-30 14:25:54 -0500

    op-build update Sep 30th
    Updated version: hostboot
    
    Tested (boot to Petitboot only) habanero, and firstone HW

--- openpower/package/hostboot/hostboot.mk
+++ openpower/package/hostboot/hostboot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOSTBOOT_VERSION ?= e3842169002e9a28947489f5a62764880898437a
+HOSTBOOT_VERSION ?= bc98d0b885dcfef970a2135c18ce7d55ab68b366
 HOSTBOOT_SITE ?= $(call github,open-power,hostboot,$(HOSTBOOT_VERSION))
 
 HOSTBOOT_LICENSE = Apache-2.0
