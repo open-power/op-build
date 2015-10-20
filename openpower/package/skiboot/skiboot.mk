c0dc04102b90a1aa042eeda214022ec8a55cb6b7
Stewart Smith <stewart@linux.vnet.ibm.com>
2015-10-20 14:19:47 +1100

    Bump skiboot to 5.1.8
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- openpower/package/skiboot/skiboot.mk
+++ openpower/package/skiboot/skiboot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKIBOOT_VERSION = skiboot-5.1.6
+SKIBOOT_VERSION = skiboot-5.1.8
 SKIBOOT_SITE = $(call github,open-power,skiboot,$(SKIBOOT_VERSION))
 SKIBOOT_INSTALL_IMAGES = YES
 SKIBOOT_INSTALL_TARGET = NO
