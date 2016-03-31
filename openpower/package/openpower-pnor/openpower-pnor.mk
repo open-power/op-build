open-power/op-build

commit 1d0a7b00d7578aafa8c8a0a81925e501453a01f1

op-build update 3-30-2016

openpower/package/openpower-pnor/openpower-pnor.mk
@@ -8,7 +8,7 @@
 # make doesn't care for quotes in the dependencies.
 XML_PACKAGE=$(subst $\",,$(BR2_OPENPOWER_XML_PACKAGE))
 
-OPENPOWER_PNOR_VERSION ?= 1e4410052a26acf4b439226860afae9507f0d94a
+OPENPOWER_PNOR_VERSION ?= f5b523fb0a7fe312c5d011e9853f53c22a2f2262
 OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 
 OPENPOWER_PNOR_LICENSE = Apache-2.0

openpower/package/garrison-xml/garrison.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GARRISON_XML_VERSION ?= 3f926e28d514b3ff3315b4179d259580a0f8b32b
+GARRISON_XML_VERSION ?= f55807eb198d56323ab0e0767769a87ee984909f
 GARRISON_XML_SITE ?= $(call github,open-power,garrison-xml,$(GARRISON_XML_VERSION))
 
 GARRISON_XML_LICENSE = Apache-2.0

openpower/package/hostboot/hostboot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOSTBOOT_VERSION ?= 11e78f85999297fffde7cbe245c6a70aaacda9a3
+HOSTBOOT_VERSION ?= 455cfde12db59a37867f0f05b9778314af2b4b5a
 HOSTBOOT_SITE ?= $(call github,open-power,hostboot,$(HOSTBOOT_VERSION))
 
 HOSTBOOT_LICENSE = Apache-2.0

commit 07f905d68d491c2040845d4faa296be0b2942f52

openpower-pnor: revert to working version
Without corresponding updates to makefiles (not yet approved for merge),
the newer openpower-pnor package breaks building a pnor with an error
from xz.

Fixes: 1d0a7b00d7578aafa8c8a0a81925e501453a01f1
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

openpower/package/openpower-pnor/openpower-pnor.mk
@@ -8,7 +8,7 @@
 # make doesn't care for quotes in the dependencies.
 XML_PACKAGE=$(subst $\",,$(BR2_OPENPOWER_XML_PACKAGE))
 
-OPENPOWER_PNOR_VERSION ?= f5b523fb0a7fe312c5d011e9853f53c22a2f2262
+OPENPOWER_PNOR_VERSION ?= 1e4410052a26acf4b439226860afae9507f0d94a
 OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 
 OPENPOWER_PNOR_LICENSE = Apache-2.0