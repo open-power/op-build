c2e2f51394b9b43ec63fb70093f36b1501a4a2af
hostboot <hostboot@us.ibm.com>
2016-04-05 00:07:19 -0500

    op-build update 4-5-2016

--- openpower/package/hostboot/hostboot.mk
+++ openpower/package/hostboot/hostboot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOSTBOOT_VERSION ?= 32dff4ca9db6d8b088ed6ec63f147d2aaded3e69
+HOSTBOOT_VERSION ?= c02ab956d79100cb5c7c177f76d618a876c81889
 HOSTBOOT_SITE ?= $(call github,open-power,hostboot,$(HOSTBOOT_VERSION))
 
 HOSTBOOT_LICENSE = Apache-2.0
