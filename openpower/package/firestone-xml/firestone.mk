c6031c19fa54f00d4b8c61856793a6b10ed8faa8
hostboot <hostboot@us.ibm.com>
2016-07-22 00:07:46 -0500

    op-build update 7-22-2016

--- openpower/package/firestone-xml/firestone.mk
+++ openpower/package/firestone-xml/firestone.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FIRESTONE_XML_VERSION ?= 15e371874a8dec1d8ef279aef2d0a0fed3ded8dd
+FIRESTONE_XML_VERSION ?= 783df1f6efce8f0283ca6683825578d1a260cad3
 FIRESTONE_XML_SITE ?= $(call github,open-power,firestone-xml,$(FIRESTONE_XML_VERSION))
 
 FIRESTONE_XML_LICENSE = Apache-2.0
