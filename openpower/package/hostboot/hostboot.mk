0a2467d724b7f79be17f6d0536453283d6329c8c
Matt Ploetz <maploetz@us.ibm.com>
2016-08-26 17:21:06 -0500

    Add witherspoon defconfig, p9 support for hostboot

--- openpower/package/hostboot/hostboot.mk
+++ openpower/package/hostboot/hostboot.mk
@@ -3,8 +3,10 @@
 # hostboot
 #
 ################################################################################
+HOSTBOOT_VERSION_BRANCH_MASTER_P8 = 7658eab13cd23cb1ee11f66593603f42faa58b55
+HOSTBOOT_VERSION_BRANCH_MASTER = f5d53757fabb36a84abb75c2f97b66a7429f134b
 
-HOSTBOOT_VERSION ?= 7658eab13cd23cb1ee11f66593603f42faa58b55
+HOSTBOOT_VERSION ?= $(if $(BR2_OPENPOWER_POWER9),$(HOSTBOOT_VERSION_BRANCH_MASTER),$(HOSTBOOT_VERSION_BRANCH_MASTER_P8))
 HOSTBOOT_SITE ?= $(call github,open-power,hostboot,$(HOSTBOOT_VERSION))
 
 HOSTBOOT_LICENSE = Apache-2.0
@@ -16,7 +18,24 @@ HOSTBOOT_INSTALL_TARGET = NO
 HOSTBOOT_ENV_VARS=$(TARGET_MAKE_ENV) \
     CONFIG_FILE=$(BR2_EXTERNAL)/configs/hostboot/$(BR2_HOSTBOOT_CONFIG_FILE) \
     OPENPOWER_BUILD=1 CROSS_PREFIX=$(TARGET_CROSS) HOST_PREFIX="" HOST_BINUTILS_DIR=$(HOST_BINUTILS_DIR) \
-    HOSTBOOT_VERSION=`cat $(HOSTBOOT_VERSION_FILE)`
+    HOSTBOOT_VERSION=`cat $(HOSTBOOT_VERSION_FILE)` 
+
+define HOSTBOOT_APPLY_PATCHES
+       if [ "$(BR2_OPENPOWER_POWER9)" == "y" ]; then \
+           $(APPLY_PATCHES) $(@D) $(BR2_EXTERNAL)/package/hostboot/p9Patches \*.patch; \
+           if [ -d $(BR2_EXTERNAL)/custom/patches/hostboot/p9Patches ]; then \
+               $(APPLY_PATCHES) $(@D) $(BR2_EXTERNAL)/custom/patches/hostboot/p9Patches \*.patch; \
+           fi; \
+       fi; \
+       if [ "$(BR2_OPENPOWER_POWER8)" == "y" ]; then \
+           $(APPLY_PATCHES) $(@D) $(BR2_EXTERNAL)/package/hostboot/p8Patches \*.patch; \
+           if [ -d $(BR2_EXTERNAL)/custom/patches/hostboot/p8Patches ]; then \
+               $(APPLY_PATCHES) $(@D) $(BR2_EXTERNAL)/custom/patches/hostboot/p8Patches \*.patch; \
+           fi; \
+       fi;
+endef
+
+HOSTBOOT_POST_PATCH_HOOKS += HOSTBOOT_APPLY_PATCHES
 
 define HOSTBOOT_BUILD_CMDS
         $(HOSTBOOT_ENV_VARS) bash -c 'cd $(@D) && source ./env.bash && $(MAKE)'
