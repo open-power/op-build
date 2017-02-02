1ee9c4648783bff7528a94f80d473a1f25853d52
Dean Sanner <dsanner@us.ibm.com>
2017-02-02 15:04:41 -0600

    Update hostboot binaries and zaius config
      - Moved SBE and STOP image to RS4v3 format
      - Enabled IPMI
      - Enabled SBE updates
      - Enabled DVPD and PVPD collection from HW

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOSTBOOT_BINARIES_VERSION ?= 7425fb1d540920f0397f57f6c54a180f4f6a720e
+HOSTBOOT_BINARIES_VERSION ?= 541a2bd2ceffc0d2d12776e213b95b5b0201bea6
 HOSTBOOT_BINARIES_SITE ?= $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0
 HOSTBOOT_BINARIES_LICENSE_FILES = LICENSE
