ddfda7652aac6dbe43de22308d689edec479777e
hostboot <hostboot@us.ibm.com>
2017-02-28 00:07:18 -0600

    op-build update 2-28-2017

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOSTBOOT_BINARIES_VERSION ?= 446c2ea03d1359a2c4960c4c590b3886a35ccffe
+HOSTBOOT_BINARIES_VERSION ?= 940ac33324d6c2498b88ce3881a2e7644e23dc7b
 HOSTBOOT_BINARIES_SITE ?= $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0
 HOSTBOOT_BINARIES_LICENSE_FILES = LICENSE
