a29f88fcf97a1d0620cff301ba967e7b47f2e643
Sachin Gupta <sgupta2m@in.ibm.com>
2017-03-16 08:59:51 -0500

    Update SBE code for P9

---
+++ openpower/package/sbe/sbe.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# SBE
+#
+################################################################################
+
+SBE_VERSION ?= 3e4b287852cdd9177099fdb5093add068b777d61
+SBE_SITE ?= $(call github,open-power,sbe,$(SBE_VERSION))
+
+SBE_LICENSE = Apache-2.0
+SBE_DEPENDENCIES = host-ppe42-gcc
+
+SBE_INSTALL_IMAGES = YES
+SBE_INSTALL_TARGET = NO
+
+define SBE_BUILD_CMDS
+		bash -c 'cd $(@D)  && make LD_LIBRARY_PATH=$(HOST_DIR)/usr/lib CROSS_COMPILER_PATH=$(PPE42_GCC_BIN)'
+endef
+
+define SBE_INSTALL_IMAGES_CMDS
+		$(INSTALL) -D $(@D)/images/p9_ipl_build  $(HOST_DIR)/usr/bin/
+		python $(@D)/src/build/sbeOpDistribute.py --sbe_binary_dir=$(STAGING_DIR)/sbe_binaries --img_dir=$(@D)/images
+		cp $(@D)/src/build/sbeOpDistribute.py $(STAGING_DIR)/sbe_binaries/
+endef
+
+$(eval $(generic-package))
