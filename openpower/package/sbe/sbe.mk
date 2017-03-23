9ea14c78ac8ce537977069955e6f857222587f59
spashabk-in <shakeebbk@in.ibm.com>
2017-03-23 01:23:47 -0500

    SBE make and install

--- openpower/package/sbe/sbe.mk
+++ openpower/package/sbe/sbe.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-#SBE_VERSION = sgupta
-#SBE_SITE = /esw/san2/sgupta2m/sbeTest/ppe/ppe
-#SBE_SITE_METHOD = local
-SBE_VERSION ?= c48c01ff77c6e0b41c24308b21b6001b72d20c65
-SBE_SITE ?= $(call github,open-power,sbe,$(SBE_VERSION))
-
+#SBE_VERSION ?= c48c01ff77c6e0b41c24308b21b6001b72d20c65
+#SBE_SITE ?= $(call github,open-power,sbe,$(SBE_VERSION))
+SBE_SITE_METHOD = git
+SBE_SITE = ssh://spashabk-in@ralgit01.raleigh.ibm.com:29418/hw/ppe
+SBE_VERSION = refs/changes/48/38348/1
 
 SBE_LICENSE = Apache-2.0
 SBE_DEPENDENCIES = host-ppe42-gcc
@@ -18,11 +17,11 @@ SBE_INSTALL_IMAGES = YES
 SBE_INSTALL_TARGET = NO
 
 define SBE_BUILD_CMDS
-		bash -c 'cd $(@D)  && make LD_LIBRARY_PATH=$(HOST_DIR)/usr/lib PPE42PATH=$(PPE42_GCC_BIN) install'
+		bash -c 'cd $(@D)  && make LD_LIBRARY_PATH=$(HOST_DIR)/usr/lib CROSS_COMPILER_PATH=$(PPE42_GCC_BIN)'
 endef
 
-define HOST_PPE42_TEST_INSTALL_CMDS
-        bash -c 'cd $(@D)
+define SBE_INSTALL_IMAGES_CMDS
+		python $(@D)/src/build/sbeOpDistribute.py --root_dir=$(BR2_EXTERNAL_OP_BUILD_PATH) --staging_dir=$(STAGING_DIR) --img_dir=$(@D)/images --host_dir=$(HOST_DIR)
 endef
 
 $(eval $(generic-package))
