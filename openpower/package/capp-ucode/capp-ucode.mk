59ece90a7b50fba3d333a17f5a33f0d6cc679b92
Frederic Barrat <fbarrat@linux.vnet.ibm.com>
2017-04-26 10:21:25 +0200

    Add capp ucode for p9
    Signed-off-by: Frederic Barrat <fbarrat@linux.vnet.ibm.com>

--- openpower/package/capp-ucode/capp-ucode.mk
+++ openpower/package/capp-ucode/capp-ucode.mk
@@ -3,7 +3,7 @@
 # capp-ucode.mk
 #
 ################################################################################
-CAPP_UCODE_VERSION ?= 1bb7503078ed39b74a7b9c46925da75e547ceea6
+CAPP_UCODE_VERSION ?= 9c73e9f51d240847fbd150815a3b9c548aae0cb8
 CAPP_UCODE_SITE ?= $(call github,open-power,capp-ucode,$(CAPP_UCODE_VERSION))
 CAPP_UCODE_LICENSE = Apache-2.0
 CAPP_UCODE_LICENSE_FILES = NOTICES
