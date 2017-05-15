60b129db6f6d853b93b7177266c407bf8e2b54ec
hostboot <hostboot@us.ibm.com>
2017-05-15 00:08:02 -0500

    op-build update 5-15-2017

--- openpower/package/sbe/sbe.mk
+++ openpower/package/sbe/sbe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SBE_VERSION ?= 9b98de3709c65428cef2469e5d080d03dfabd61f
+SBE_VERSION ?= d770027426ba0c5d6c44fa985e9dfaf28fd6fcce
 SBE_SITE ?= $(call github,open-power,sbe,$(SBE_VERSION))
 
 SBE_LICENSE = Apache-2.0
