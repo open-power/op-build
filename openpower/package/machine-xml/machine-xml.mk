cd986e4cf84a526ee9653f1282e7889a9312f658
Stewart Smith <stewart@linux.vnet.ibm.com>
2017-05-15 12:14:59 +1000

    Single XML package rather than one per platform
    This saves us a *lot* of copy&pasted build foo and replaces it with a couple
    of config options that can be set in menuconfig.
    
    This makes adding a new platform a much smaller operation, requiring fewer
    changes that are scattered throughout the buildroot tree.
    
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- openpower/package/machine-xml/machine-xml.mk
+++ openpower/package/machine-xml/machine-xml.mk
@@ -1,24 +1,29 @@
 ################################################################################
-#
-# romulus_xml
+# machine_xml
 #
 ################################################################################
 
-ROMULUS_XML_VERSION ?= 301279cbdb3595ad1bbdce4d9d2189db0470a62a
-ROMULUS_XML_SITE ?= $(call github,open-power,romulus-xml,$(ROMULUS_XML_VERSION))
+MACHINE_XML_VERSION ?= $(BR2_OPENPOWER_MACHINE_XML_VERSION)
+ifeq ($(BR2_OPENPOWER_MACHINE_XML_GITHUB_PROJECT),y)
+MACHINE_XML_SITE = $(call github,open-power,$(BR2_OPENPOWER_MACHINE_XML_GITHUB_PROJECT_VALUE),$(MACHINE_XML_VERSION))
+else ifeq ($(BR2_OPENPOWER_MACHINE_XML_CUSTOM_GIT),y)
+MACHINE_XML_SITE_METHOD = git
+MACHINE_XML_SITE = $(BR2_OPENPOWER_MACHINE_XML_CUSTOM_GIT_VALUE)
+endif
 
-ROMULUS_XML_LICENSE = Apache-2.0
-ROMULUS_XML_DEPENDENCIES = hostboot-install-images openpower-mrw-install-images common-p8-xml-install-images
+MACHINE_XML_LICENSE = Apache-2.0
+MACHINE_XML_LICENSE_FILES = LICENSE
+MACHINE_XML_DEPENDENCIES = hostboot openpower-mrw common-p8-xml
 
-ROMULUS_XML_INSTALL_IMAGES = YES
-ROMULUS_XML_INSTALL_TARGET = YES
+MACHINE_XML_INSTALL_IMAGES = YES
+MACHINE_XML_INSTALL_TARGET = YES
 
 MRW_SCRATCH=$(STAGING_DIR)/openpower_mrw_scratch
 MRW_HB_TOOLS=$(STAGING_DIR)/hostboot_build_images
 
 # Defines for BIOS metadata creation
 BIOS_SCHEMA_FILE = $(MRW_HB_TOOLS)/bios.xsd
-ROMULUS_BIOS_XML_CONFIG_FILE = $(MRW_SCRATCH)/$(BR2_ROMULUS_BIOS_XML_FILENAME)
+OPENPOWER_BIOS_XML_CONFIG_FILE = $(MRW_SCRATCH)/$(BR2_OPENPOWER_BIOS_XML_FILENAME)
 BIOS_XML_METADATA_FILE = \
     $(MRW_HB_TOOLS)/$(BR2_OPENPOWER_CONFIG_NAME)_bios_metadata.xml
 PETITBOOT_XSLT_FILE = $(MRW_HB_TOOLS)/bios_metadata_petitboot.xslt
@@ -27,36 +32,48 @@ PETITBOOT_BIOS_XML_METADATA_FILE = \
 PETITBOOT_BIOS_XML_METADATA_INITRAMFS_FILE = \
     $(TARGET_DIR)/usr/share/bios_metadata.xml
 
-define ROMULUS_XML_BUILD_CMDS
-        # copy the romulus xml where the common lives
-        bash -c 'mkdir -p $(MRW_SCRATCH) && cp -r $(@D)/* $(MRW_SCRATCH)'
+ifeq ($(BR2_OPENPOWER_MACHINE_XML_OPPOWERVM_ATTRIBUTES),y)
+MACHINE_XML_OPPOWERVM_ATTR_XML = $(MRW_HB_TOOLS)/attribute_types_oppowervm.xml
+MACHINE_XML_OPPOWERVM_TARGET_XML = $(MRW_HB_TOOLS)/target_types_oppowervm.xml
+endif
+ifeq ($(BR2_OPENPOWER_MACHINE_XML_TARGET_TYPES_OPENPOWER_XML),y)
+MACHINE_XML_TARGET_TYPES_OPENPOWER_XML = $(MRW_HB_TOOLS)/target_types_openpower.xml
+endif
 
-        # generate the system mrw xml
-        perl -I $(MRW_HB_TOOLS) \
-        $(MRW_HB_TOOLS)/processMrw.pl -x $(MRW_SCRATCH)/romulus.xml
-
-        chmod +x $(MRW_HB_TOOLS)/filter_out_unwanted_attributes.pl
+define MACHINE_XML_FILTER_UNWANTED_ATTRIBUTES
+       chmod +x $(MRW_HB_TOOLS)/filter_out_unwanted_attributes.pl
 
        $(MRW_HB_TOOLS)/filter_out_unwanted_attributes.pl \
             --tgt-xml $(MRW_HB_TOOLS)/target_types_merged.xml \
             --tgt-xml $(MRW_HB_TOOLS)/target_types_hb.xml \
             --tgt-xml $(MRW_HB_TOOLS)/target_types_oppowervm.xml \
             --tgt-xml $(MRW_HB_TOOLS)/target_types_openpower.xml \
-            --mrw-xml $(MRW_SCRATCH)/ROMULUS_hb.mrw.xml
+            --mrw-xml $(MRW_SCRATCH)/$(BR2_OPENPOWER_MRW_XML_FILENAME)
 
-       cp  $(MRW_SCRATCH)/ROMULUS_hb.mrw.xml.updated  $(MRW_SCRATCH)/ROMULUS_hb.mrw.xml
+       cp  $(MRW_SCRATCH)/$(BR2_OPENPOWER_MRW_XML_FILENAME).updated  $(MRW_SCRATCH)/$(BR2_OPENPOWER_MRW_XML_FILENAME)
+endef
+
+define MACHINE_XML_BUILD_CMDS
+        # copy the machine xml where the common lives
+        bash -c 'mkdir -p $(MRW_SCRATCH) && cp -r $(@D)/* $(MRW_SCRATCH)'
+
+        # generate the system mrw xml
+        perl -I $(MRW_HB_TOOLS) \
+        $(MRW_HB_TOOLS)/processMrw.pl -x $(MRW_SCRATCH)/$(BR2_OPENPOWER_MACHINE_XML_FILENAME)
+
+	$(if $(BR2_OPENPOWER_MACHINE_XML_FILTER_UNWANTED_ATTRIBUTES), $(call MACHINE_XML_FILTER_UNWANTED_ATTRIBUTES))
 
         # merge in any system specific attributes, hostboot attributes
-        $(MRW_HB_TOOLS)/mergexml.sh $(MRW_SCRATCH)/$(BR2_ROMULUS_SYSTEM_XML_FILENAME) \
+        $(MRW_HB_TOOLS)/mergexml.sh $(MRW_SCRATCH)/$(BR2_OPENPOWER_SYSTEM_XML_FILENAME) \
             $(MRW_HB_TOOLS)/attribute_types.xml \
             $(MRW_HB_TOOLS)/attribute_types_hb.xml \
-            $(MRW_HB_TOOLS)/attribute_types_oppowervm.xml \
+            $(MACHINE_XML_OPPOWERVM_ATTR_XML) \
             $(MRW_HB_TOOLS)/attribute_types_openpower.xml \
             $(MRW_HB_TOOLS)/target_types_merged.xml \
             $(MRW_HB_TOOLS)/target_types_hb.xml \
-            $(MRW_HB_TOOLS)/target_types_oppowervm.xml \
-            $(MRW_HB_TOOLS)/target_types_openpower.xml \
-            $(MRW_SCRATCH)/$(BR2_ROMULUS_MRW_XML_FILENAME) > $(MRW_HB_TOOLS)/temporary_hb.hb.xml;
+            $(MACHINE_XML_OPPOWERVM_TARGET_XML) \
+            $(MACHINE_XML_TARGET_TYPES_OPENPOWER_XML) \
+            $(MRW_SCRATCH)/$(BR2_OPENPOWER_MRW_XML_FILENAME) > $(MRW_HB_TOOLS)/temporary_hb.hb.xml;
 
         # creating the targeting binary
         $(MRW_HB_TOOLS)/xmltohb.pl  \
@@ -65,7 +82,7 @@ define ROMULUS_XML_BUILD_CMDS
             --src-output-dir=none \
             --img-output-dir=$(MRW_HB_TOOLS)/ \
             --vmm-consts-file=$(MRW_HB_TOOLS)/vmmconst.h --noshort-enums \
-            --bios-xml-file=$(ROMULUS_BIOS_XML_CONFIG_FILE) \
+            --bios-xml-file=$(OPENPOWER_BIOS_XML_CONFIG_FILE) \
             --bios-schema-file=$(BIOS_SCHEMA_FILE) \
             --bios-output-file=$(BIOS_XML_METADATA_FILE)
 
@@ -76,11 +93,11 @@ define ROMULUS_XML_BUILD_CMDS
             $(BIOS_XML_METADATA_FILE)
 endef
 
-define ROMULUS_XML_INSTALL_IMAGES_CMDS
+define MACHINE_XML_INSTALL_IMAGES_CMDS
         mv $(MRW_HB_TOOLS)/targeting.bin $(MRW_HB_TOOLS)/$(BR2_OPENPOWER_TARGETING_BIN_FILENAME)
 endef
 
-define ROMULUS_XML_INSTALL_TARGET_CMDS
+define MACHINE_XML_INSTALL_TARGET_CMDS
         # Install Petitboot specific BIOS XML into initramfs's usr/share/ dir
         $(INSTALL) -D -m 0644 \
             $(PETITBOOT_BIOS_XML_METADATA_FILE) \
