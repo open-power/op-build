25b20560c65cb2b411e3812080651c05268e21af
Bill Hoffa <wghoffa@us.ibm.com>
2017-06-09 12:31:21 -0500

    op-build update 06-09-2017

--- openpower/package/occ/occ.mk
+++ openpower/package/occ/occ.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 OCC_VERSION_BRANCH_MASTER_P8 ?= 28f2cec690b7f19548ce860a8820f519e6c39a6a
-OCC_VERSION_BRANCH_MASTER ?= bc34ad5806549a7a20079745b2b1f8ef439ade0c
+OCC_VERSION_BRANCH_MASTER ?= 852d3b94da35cfa28c08f871df6eff4a21681d60
 
 OCC_VERSION ?= $(if $(BR2_OPENPOWER_POWER9),$(OCC_VERSION_BRANCH_MASTER),$(OCC_VERSION_BRANCH_MASTER_P8))
 OCC_SITE ?= $(call github,open-power,occ,$(OCC_VERSION))
