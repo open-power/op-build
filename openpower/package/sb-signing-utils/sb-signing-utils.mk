e7da8bce4b0e985d2eec172994256687a3fc4caa
Dave Heller <hellerda@linux.vnet.ibm.com>
2017-06-16 18:30:40 -0400

    Integrate sb-signing-utils into op-build
    This adds support for building sb-signing-utils from github.com/hellerda.
    
    It includes the openpower/package/sb-signing-utils/ directory with Config.in,
    *.mk, and ./keys directory holding the IBM Imprint keys and README.  Build is
    activated by the presence of BR2_OPENPOWER_SECUREBOOT_ENABLED in the _defconfig.
    In this patch it is set in habanero_defconfig only.
    
    Even with BR2_OPENPOWER_SECUREBOOT_ENABLED, the package will only be built, it
    will not be activated (i.e. used in PNOR packaging step) until additional
    patches are added to op-build and openpower-pnor packages.
    
    Signed-off-by: Dave Heller <hellerda@linux.vnet.ibm.com>
    [stewart@linux.vnet.ibm.com: cleaned git history, cherry-picked onto master]
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

---
+++ openpower/package/sb-signing-utils/sb-signing-utils.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+#  sb-signing-utils
+#
+################################################################################
+
+SB_SIGNING_UTILS_SITE ?= $(call github,open-power,sb-signing-utils,$(SB_SIGNING_UTILS_VERSION))
+
+SB_SIGNING_UTILS_LICENSE = Apache-2.0
+SB_SIGNING_UTILS_LICENSE_FILES = LICENSE
+SB_SIGNING_UTILS_VERSION ?= v0.2
+
+HOST_SB_SIGNING_UTILS_DEPENDENCIES = host-openssl
+
+HOST_SB_SIGNING_UTILS_AUTORECONF = YES
+HOST_SB_SIGNING_UTILS_AUTORECONF_OPTS = -i
+
+define HOST_SB_SIGNING_UTILS_COPY_FILES
+	$(INSTALL) -m 0755 $(@D)/crtSignedContainer.sh $(HOST_DIR)/usr/bin/
+endef
+
+SB_SIGNING_UTILS_KEY_SRC_PATH=$(BR2_EXTERNAL)/package/sb-signing-utils/keys
+SB_SIGNING_UTILS_KEY_DST_PATH=$(HOST_DIR)/etc/keys
+
+define HOST_SB_SIGNING_UTILS_COPY_KEYS
+	$(INSTALL) -d -m 0755 $(SB_SIGNING_UTILS_KEY_DST_PATH)
+	$(INSTALL) -m 0755 $(SB_SIGNING_UTILS_KEY_SRC_PATH)/* \
+		$(SB_SIGNING_UTILS_KEY_DST_PATH)
+endef
+
+HOST_SB_SIGNING_UTILS_POST_INSTALL_HOOKS += HOST_SB_SIGNING_UTILS_COPY_FILES
+HOST_SB_SIGNING_UTILS_POST_INSTALL_HOOKS += HOST_SB_SIGNING_UTILS_COPY_KEYS
+
+$(eval $(host-autotools-package))
