f7b72cec153bb233c557ae7885bf545d52f59473
hostboot <hostboot@us.ibm.com>
2017-09-06 13:30:17 -0500

    op-build update 9-6-2017
    Changes Included for occ:
    6f77ee7 - Sooraj Nair - 2017-09-06 - temporary fix for sw399904
    4b775f5 - William Bryan - 2017-09-05 - Fix I2C locking issue and sensor IDs
    f6b9c4c - Andres Lugo-Reyes - 2017-09-01 - Refine WOF calculations
    e1a597e - mbroyles - 2017-08-29 - Calculate GPU Power Cap
    c34f286 - mbroyles - 2017-08-29 - Enable safe mode memory throttling

--- openpower/package/occ/occ.mk
+++ openpower/package/occ/occ.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 OCC_VERSION_BRANCH_MASTER_P8 ?= 28f2cec690b7f19548ce860a8820f519e6c39a6a
-OCC_VERSION_BRANCH_MASTER ?= 854999aaca9b49263df09248fb830a9513a381d4
+OCC_VERSION_BRANCH_MASTER ?= 6f77ee7bfbcc7e36fe55ee2891c4372bd5a0143a
 
 OCC_VERSION ?= $(if $(BR2_OPENPOWER_POWER9),$(OCC_VERSION_BRANCH_MASTER),$(OCC_VERSION_BRANCH_MASTER_P8))
 OCC_SITE ?= $(call github,open-power,occ,$(OCC_VERSION))
