61faba24aaa736e52bac4ec1a2eb049bdd0074e8
hostboot <hostboot@us.ibm.com>
2017-09-14 01:40:21 -0500

    op-build update 9-14-2017
    Changes Included for zaius-xml:
    9325c75 - Adrian Barrera - 2017-09-12 - Update WOF tables
    187091f - Adrian Barrera - 2017-09-12 - Add tpm_required_sensor
    
    Changes Included for hostboot:
    3d6c541 - nagurram-in - 2017-09-13 - Fixed slot map entry id's,vendor id and some other stuff
    8d573cb - Ilya Smirnov - 2017-09-13 - Add attrInfo.csv file into hostboot_build_images directory
    c55d21d - Swathi Madhuri Bhattiprolu - 2017-09-13 - update the Targets.pm scripts to fix issues found during review
    aed2bbf - Christian Geddes - 2017-09-13 - Fix RTC numbers for extra core state setup we do for MPIPL
    c6ca45b - David Kauer - 2017-09-13 - INT scom initfile update
    339026b - Jacob Harvey - 2017-09-13 - Disabled Training Advance in sim
    08c425e - Lennard Streat - 2017-09-13 - Expanding MCU tag fifo settings to be freq dependent.
    99fdea2 - Dan Crowell - 2017-09-13 - Remove hack to override MRW enablement of WOF on ZZ
    14b8cdd - Christian Geddes - 2017-09-12 - Remove setting of WAKEUP_SELECT bit on QPPM register during MPIPL
    3b784aa - Dan Crowell - 2017-09-12 - Default to use the master's side for slave sbe starts in OP
    753699c - Dan Crowell - 2017-09-12 - Move a couple xbus HWPs into PRD library
    bba3b32 - Dan Crowell - 2017-09-12 - Enable calls to p9_mss_draminit_training_adv
    77261b6 - Dan Crowell - 2017-09-12 - Add sensor name to IPMI sensor error logs
    
    Changes Included for pnor:
    824989f - e-liner - 2017-09-13 - Merge pull request #81 from e-liner/memd_size
    9b082d0 - Elizabeth Liner - 2017-09-13 - Fixing MEMD size

--- openpower/package/openpower-pnor/openpower-pnor.mk
+++ openpower/package/openpower-pnor/openpower-pnor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_VERSION ?= 3b22d6343a1984dd5ae9a555491771aecdaa2cc0
+OPENPOWER_PNOR_VERSION ?= 824989ffd21a300363de4871aa0e6f286d7f6ce0
 OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 
 OPENPOWER_PNOR_LICENSE = Apache-2.0
