9871bd710c4a2e0ce600d03896f69496378cc048
hostboot <hostboot@us.ibm.com>
2017-10-10 21:22:40 -0500

    op-build update 10-10-2017
    Changes Included for package hostboot, branch master:
    b01e6bb - Roland Veloz - 2017-10-10 - In VPD::sendMboxWriteMsg, do a noop if no SP Base Services available
    9c39127 - Marty Gloff - 2017-10-10 - Bringup memory remapping for no memory behind master processor
    c381bea - Elizabeth Liner - 2017-10-10 - Moving SBE error and threshold handler to sbeio
    5173779 - Christian Geddes - 2017-10-10 - Enable sbe test cases
    a6aecde - Christian Geddes - 2017-10-10 - Enable mbox test cases
    c411e8e - Nick Bofferding - 2017-10-10 - Secure Boot: Support __getkey option in production signing
    784ba67 - Stephen Cprek - 2017-10-10 - Add std::map operator== overload and != for vector
    0213310 - Christian Geddes - 2017-10-10 - Remove workaround clearning interrpt register
    bc5682a - Nick Bofferding - 2017-10-10 - Secure Boot: Remove requirement to specify production config file
    866719e - Prachi Gupta - 2017-10-10 - update bbuild to  b0911a to pull in cumulus.pnor from bbuild
    48cf6f7 - Elizabeth Liner - 2017-10-10 - Specifying which machine we're building for memd changes
    95cbd5c - Greg Still - 2017-10-10 - PM: Change FFDC for SGPE and PGPE based on failure error log debug
    674ff56 - Sangeetha T S - 2017-10-10 - Level 3: Stop GPE, PGPE procedures
    6145e9e - Brian Vanderpool - 2017-10-10 - Add configuration error checking before starting SGPE
    3600884 - Prasad Bg Ranganath - 2017-10-10 - P9 Pstate: VREF calibration suport
    be8af0a - Sangeetha T S - 2017-10-10 - Register data collection in FFDC
    1bfc4a2 - Sangeetha T S - 2017-10-10 - Register FFDC collection for init/reset of Stop and OCC GPEs.
    71221a9 - Amit Kumar - 2017-10-10 - L2 stop_gpe_init
    5c997ea - Dan Crowell - 2017-10-10 - Temp remove p9_pm_stop_gpe_init_errors.xml to fix mirror glitch
    475bc8d - Thi Tran - 2017-10-10 - Cumulus: support MCFGPA_HOLE0_UPPER_ADDRESS_AT_END_OF_RANGE bit setting
    
    Changes Included for package hostboot-binaries, branch master:
    d9d2910 - William Hoffa - 2017-10-10 - Merge pull request #50 from wghoffa/master
    44b6de1 - Bill Hoffa - 2017-10-10 - Update to GPU GPE1 Binary 10/3

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 
-HOSTBOOT_BINARIES_VERSION ?= ed06137f062d6a716d2a89d852aac984036db256
+HOSTBOOT_BINARIES_VERSION ?= d9d2910d113915002811d5fa31eed1ec17ca785d
 HOSTBOOT_BINARIES_SITE ?= $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0
