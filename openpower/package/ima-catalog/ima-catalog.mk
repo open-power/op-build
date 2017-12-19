c390bcbf030ed2f77d6ce78a9519e099c59ed6b8
Deepak K Gangadhar <deepakg@in.ibm.com>
2017-12-19 06:19:50 -0600

    Add event description for PowerBus and MCS events in POWER9 DTS files
    Signed-off-by: Deepak K Gangadhar <deepakg@in.ibm.com>

--- openpower/package/ima-catalog/ima-catalog.mk
+++ openpower/package/ima-catalog/ima-catalog.mk
@@ -3,7 +3,7 @@
 # ima-catalog.mk
 #
 ################################################################################
-IMA_CATALOG_VERSION ?= dd647bf317ecccd9bf13fb1c7aa50d2fb6c44980
+IMA_CATALOG_VERSION ?= 01b26a136da16a87c0b6b3c4d9f27555dca104dc 
 IMA_CATALOG_SITE ?= $(call github,open-power,ima-catalog,$(IMA_CATALOG_VERSION))
 IMA_CATALOG_LICENSE = Apache-2.0
 IMA_CATALOG_DEPENDENCIES = host-dtc host-xz
