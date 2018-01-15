open-power/op-build

commit 735aec595bf418dc9050671be74db7bc08b66a39

op-build update 12-6-2017
Changes Included for package zaius-xml, branch master:
c254d1e - Adrian Barrera - 2017-12-01 - Add 23c 225W WOF Table
1173666 - Adrian Barrera - 2017-11-15 - Move 25G-OC clock select to IO filter PLL

Changes Included for package witherspoon-xml, branch master:
11e6ca0 - William Hoffa - 2017-12-05 - Merge pull request #74 from wghoffa/dd21_stabilization
5806543 - Bill Hoffa - 2017-12-05 - Stabilize DD2.1 by disabling some stop states and disable VDM + WOF
32bfab1 - Erich Hauptli - 2017-11-22 - Adding OPEN_POWER_N_PLUS_ONE_HPC_BULK_POWER_LIMIT_WATTS
f0ef411 - Erich Hauptli - 2017-11-17 - Adding in additional WOF table

Changes Included for package hostboot, branch master:
665c9a1 - Corey Swenson - 2017-12-05 - Fix for MDIA timeouts on Cumulus step 14.1
5b5972e - Stephen Cprek - 2017-12-05 - Create a Trusted boot trace component and collect trace on errors
ca52131 - Stephen Cprek - 2017-12-05 - Handle ContainerHeader asserts more nicely with error logs
89f7297 - Matt Derksen - 2017-12-05 - Test and verify concurrent init of PM complex
c71ed1c - Dzuy Nguyen - 2017-12-01 - Add support for OBUS PLL buckets
6bc72e2 - Christian Geddes - 2017-12-01 - Remove limit on max size of PNOR TOC
3c9fe01 - Brian Vanderpool - 2017-12-01 - PM: Mask CME LFIR Bits 13:19 - they are only used for characterization
453d38a - Dan Crowell - 2017-12-01 - Revert ATTR_CEN_ECID back to ATTR_ECID
716a165 - Joel Stanley - 2017-12-01 - fapi2: Fix template call sites for GCC 7
f7b6ad2 - Robert Lippert - 2017-12-01 - hdat: use VRML record for processor PCRD VPD data
4319e2a - Prasad Bg Ranganath - 2017-11-30 - p9_pm_pstate_gpe_init: setup Fsafe in all configured QPPMs
1c06b1a - Prasad Bg Ranganath - 2017-11-30 - p9_setup_evid: Missed applying bias values for pound V
385c30e - Claus Michael Olsen - 2017-11-30 - Support for PPE commit 49883 to error out on non-TOR ring section.
9b4e254 - Benjamin Weisenbeck - 2017-11-30 - Call xbus restore erepair in istep 9

openpower/package/openpower-pnor/openpower-pnor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_VERSION ?= 5181154715dd12e10f1d1ac432e128f44e8d32c0
+OPENPOWER_PNOR_VERSION ?= 4c844a5ef7efc6605c4c877b6c92cf0a1a0df0b5
 OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 
 OPENPOWER_PNOR_LICENSE = Apache-2.0

openpower/configs/witherspoon_defconfig
@@ -41,7 +41,7 @@ BR2_OPENPOWER_PLATFORM=y
 BR2_OPENPOWER_POWER9=y
 BR2_HOSTBOOT_CONFIG_FILE="witherspoon.config"
 BR2_OPENPOWER_MACHINE_XML_GITHUB_PROJECT_VALUE="witherspoon-xml"
-BR2_OPENPOWER_MACHINE_XML_VERSION="882ef2310daa6d5f6e38a93e0722c83bc8e8e52d"
+BR2_OPENPOWER_MACHINE_XML_VERSION="11e6ca068e718765f2e2de42f7967227819a5d14"
 BR2_OPENPOWER_MACHINE_XML_FILENAME="witherspoon.xml"
 BR2_OPENPOWER_SYSTEM_XML_FILENAME="WITHERSPOON_hb.system.xml"
 BR2_OPENPOWER_MRW_XML_FILENAME="WITHERSPOON_hb.mrw.xml"

openpower/configs/zaius_defconfig
@@ -41,7 +41,7 @@ BR2_OPENPOWER_PLATFORM=y
 BR2_OPENPOWER_POWER9=y
 BR2_HOSTBOOT_CONFIG_FILE="zaius.config"
 BR2_OPENPOWER_MACHINE_XML_GITHUB_PROJECT_VALUE="zaius-xml"
-BR2_OPENPOWER_MACHINE_XML_VERSION="338671f00ca8b950bfc72f6c9728c68c1029a1b8"
+BR2_OPENPOWER_MACHINE_XML_VERSION="c254d1ecc0d7ffebdfc00fff7f8d41d62390760a"
 BR2_OPENPOWER_MACHINE_XML_FILENAME="zaius.xml"
 BR2_OPENPOWER_SYSTEM_XML_FILENAME="ZAIUS_hb.system.xml"
 BR2_OPENPOWER_MRW_XML_FILENAME="ZAIUS_hb.mrw.xml"

openpower/package/hostboot/hostboot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 HOSTBOOT_VERSION_BRANCH_MASTER_P8 ?= 695bd891343faf1f0ef85fe53148590e58239efd
-HOSTBOOT_VERSION_BRANCH_MASTER ?= 9a2e268f1799061d8eedaadd4aedc7c54cc6589e
+HOSTBOOT_VERSION_BRANCH_MASTER ?= 665c9a18838f155ae32c74c829fd4605bfd8d30a
 
 HOSTBOOT_VERSION ?= $(if $(BR2_OPENPOWER_POWER9),$(HOSTBOOT_VERSION_BRANCH_MASTER),$(HOSTBOOT_VERSION_BRANCH_MASTER_P8))
 HOSTBOOT_SITE ?= $(call github,open-power,hostboot,$(HOSTBOOT_VERSION))

commit 24bde45007f33e95a27fc9aed4f2f90605e4d879

PNOR fix for P8
Fixes: 735aec595bf418dc9050671be74db7bc08b66a39
Fixes: https://github.com/open-power/op-build/issues/1711
Fixes: https://github.com/open-power/pnor/commit/dccb9d427b9fb95b08af814218d06d9bee5392e6
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

openpower/package/openpower-pnor/openpower-pnor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_VERSION ?= 4c844a5ef7efc6605c4c877b6c92cf0a1a0df0b5
+OPENPOWER_PNOR_VERSION ?= b7aad501e62c36f2e7341f4e1101969bad2e8f21
 OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 
 OPENPOWER_PNOR_LICENSE = Apache-2.0