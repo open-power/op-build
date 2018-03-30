1fc80f651f4a1e5ed431799c58fc18657f1fd34e
hostboot <hostboot@us.ibm.com>
2018-03-30 17:24:52 -0500

    op-build update 3-30-2018
    Changes Included for package witherspoon-xml, branch master:
    5005334 - Erich Hauptli - 2018-03-29 - TPM FRU Addition and WOF p2 enablement
    
    Changes Included for package hostboot, branch master:
    711723b - Dan Crowell - 2018-03-30 - Ignore dummy files when parsing error log data
    5139c57 - Joe McGill - 2018-03-30 - create shells for IO OBUS pre, post training HWPs
    e507de1 - Chris Steffen - 2018-03-30 - I/O Metadata Cleanup
    225f409 - Joe McGill - 2018-03-30 - update HWP level metadata for nest, common files
    f903901 - Chris Steffen - 2018-03-30 - Initial Abus Commit
    69bd6e4 - Joe McGill - 2018-03-30 - L2 - Fabric updates for multi-chip support
    ae7ba42 - Caleb Palmer - 2018-03-30 - PRD: Enable threshold for IPL RCD parity err
    a219839 - Zane Shelley - 2018-03-30 - PRD: support getMemAddrRange() for MBA ranks
    39489bc - Caleb Palmer - 2018-03-30 - PRD: Continue looking for attns after SCOM fail on mask/act regs
    fc2a044 - Christian Geddes - 2018-03-29 - Ensure all hbmutex attributes get re-initialized on MPIPL
    46cd995 - Zane Shelley - 2018-03-29 - PRD: Disable Dynamic Memory Deallocation for MBA
    a6bd3b6 - Christian Geddes - 2018-03-29 - Allow platHwasErrorAddHWCollout now that FSP supports it
    a17b84a - Bill Hoffa - 2018-03-29 - Enable FAPI Cumulus test cases
    90eaed6 - Dan Crowell - 2018-03-29 - Force checkstops for unhandled machine checks
    284cebd - Matt Derksen - 2018-03-29 - Change deconfig rules to allow for Zeppelin proc config
    95c1dd7 - Mike Baiocchi - 2018-03-29 - Close and Disable TCEs on Non-Master Nodes
    42e4c42 - Zane Shelley - 2018-03-29 - PRD: moved prdfCenMbaDataBundle.H to common code
    0bd003a - Sameer Veer - 2018-03-28 - Code cleanup - removed test-code not required in prod
    46b6c68 - Louis Stermole - 2018-03-28 - Add empty p9c delayRegs.H for hostboot
    9ef8d33 - Sameer Veer - 2018-03-28 - Integrate track before fsp-CI run triggers
    9440862 - Prachi Gupta - 2018-03-28 - attrrp_rt.C: translateAddr returns input address by default
    08b8dd5 - Caleb Palmer - 2018-03-27 - Add accessor functions for row repair attr
    92069bb - Caleb Palmer - 2018-03-27 - Add get/set functionality for row repair attr
    579e63b - Corey Swenson - 2018-03-27 - Add support for ATTR_FREQ_MCA_MHZ
    8a783ea - Corey Swenson - 2018-03-27 - Move runtime scoms to common function
    3d9454e - Thi Tran - 2018-03-26 - Do not apply HW414958 to Axone
    c4bd7604 - Soma BhanuTej - 2018-03-26 - Mask off bit 26 of TP_LFIR on FSP machines
    fe61cf0 - Mike Baiocchi - 2018-03-26 - Setup Node-Level Attributes for Multinode TCE Support
    a7decd2 - Nicholas E. Bofferding - 2018-03-26 - Revert "Check the Section Headers in Non-Secure Mode"
    b2bffd2 - Dan Crowell - 2018-03-26 - Ensure ATTR_EC_GARD and ATTR_EQ_GARD are updated before SBE update
    a5fb712 - Jenny Huynh - 2018-03-23 - HW438727 Disable clockgate to allow correct ODL error reporting
    d08fdc0 - Nick Klazynski - 2018-03-23 - Remove CDD1.1 security IMC; Apply indirect branch serialization to HV=0 only
    3a4d063 - Tsung Yeung - 2018-03-23 - Adds STR entry and exit functions to support NVDIMM
    ec53527 - Prasad Bg Ranganath - 2018-03-23 - PPB: Update occ min frequency with real driven value
    b8da2d8 - Joachim Fenkes - 2018-03-23 - p9_sbe_lpc_init: Fix timeout setup
    eea4b09 - Richard J. Knight - 2018-03-23 - Fix missing set_XX method for sbeTarget callout
    a4d47c4 - Dan Crowell - 2018-03-23 - Support Nimbus DD2.3 and Cumulus 1.2 PLL Buckets
    
    Changes Included for package hostboot-binaries, branch master:

--- openpower/package/hostboot-binaries/hostboot_binaries.mk
+++ openpower/package/hostboot-binaries/hostboot_binaries.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 
-HOSTBOOT_BINARIES_VERSION ?= 2657e58ac28a32c73a3002c715b6ea9d8880f112
+HOSTBOOT_BINARIES_VERSION ?= b339e05c57725cf09b867d6652698aa2e3ab5f6a
 HOSTBOOT_BINARIES_SITE ?= $(call github,open-power,hostboot-binaries,$(HOSTBOOT_BINARIES_VERSION))
 
 HOSTBOOT_BINARIES_LICENSE = Apache-2.0
