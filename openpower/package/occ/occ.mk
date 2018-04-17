d69f08f71484c5b855d7dbbabdaed9f0111182ff
hostboot <hostboot@us.ibm.com>
2018-04-17 02:30:32 -0500

    op-build update 4-17-2018
    Changes Included for package occ, branch master:
    4f49f63 - mbroyles - 2018-04-16 - AVSBus Vdd Current roll over workaround
    bee2601 - mbroyles - 2018-04-12 - Remove disable of Pstates on a transition to standby state

--- openpower/package/occ/occ.mk
+++ openpower/package/occ/occ.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 OCC_VERSION_BRANCH_MASTER_P8 ?= 28f2cec690b7f19548ce860a8820f519e6c39a6a
-OCC_VERSION_BRANCH_MASTER ?= 6c9f28ebe2596588ff6cc819bc4c2c158755ca43
+OCC_VERSION_BRANCH_MASTER ?= 4f49f6351fa3908703b91e0dea9f3680c77a47a6
 
 OCC_VERSION ?= $(if $(BR2_OPENPOWER_POWER9),$(OCC_VERSION_BRANCH_MASTER),$(OCC_VERSION_BRANCH_MASTER_P8))
 OCC_SITE ?= $(call github,open-power,occ,$(OCC_VERSION))
