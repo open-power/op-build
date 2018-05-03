e2a463506a54c02e5dd36328d8b66842d2839124
Stewart Smith <stewart@linux.ibm.com>
2018-05-02 20:16:09 -0500

    hcode,hostboot-binaries,hostboot,openpower-pnor bump
    Changes Included for package hcode, branch master:
    b6c5b85 - hostboot - 2018-05-01 - Release tag information updated for hw050118a.920
    7d272ae - Claus Michael Olsen - 2018-05-01 - Risk level 3/4/5 support: Step 2 - image update to TOR v7
    
    Changes Included for package hostboot, branch master:
    45dddee - Stephen Glancy - 2018-05-01 - Adds blank files for DMI IO
    0b487eb - hostboot - 2018-05-01 - Remove new files of manually-mirrored-commit ad4459f
    
    Changes Included for package pnor, branch master:
    f6d970c - Dan - 2018-05-01 - Merge pull request #96 from cvswen/update_pnor
    641bb28 - Corey Swenson - 2018-04-26 - Make VPD sections preserved
    
    Changes Included for package hostboot-binaries, branch master:
    69b3c1c - hostboot - 2018-05-01 - UPDATE executables for the NVIDIA gpu and/or ring files for hw050118a.920
    
    Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

--- openpower/package/openpower-pnor/openpower-pnor.mk
+++ openpower/package/openpower-pnor/openpower-pnor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_VERSION ?= 864bec404e8d5a5ee19e3d5478b290cdf6ef7cab
+OPENPOWER_PNOR_VERSION ?= f6d970c6a600a7e248fa5d604eb471db4482760b
 OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 
 OPENPOWER_PNOR_LICENSE = Apache-2.0
