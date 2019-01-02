53eadf2ad57534830300f8973110034d6c5bc47a
hostboot <hostboot@us.ibm.com>
2019-01-02 00:44:47 -0600

    op-build update 1-2-2019
    Changes Included for package hostboot, branch master-p8:
    994e324 - Mike Baiocchi - 2018-12-20 - Add Slave-Procs-Only SBE Update via FSI I2C in istep 6.9
    
    Changes Included for package hcode, branch master:
    8e3228b - hostboot - 2018-12-31 - Release tag information updated for hw123119a.930
    896327d - hostboot - 2018-12-20 - Release tag information updated for hw122018a.930
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/hostboot-p8/hostboot-p8.mk
+++ openpower/package/hostboot-p8/hostboot-p8.mk
@@ -3,7 +3,7 @@
 # hostboot for POWER8
 #
 ################################################################################
-HOSTBOOT_P8_VERSION ?= e9b07d61750eb47fd57e89472a68dc0ab8dfc01b
+HOSTBOOT_P8_VERSION ?= 994e324fe94c78bbabb3586785f374b4a3738d27
 
 HOSTBOOT_P8_SITE ?= $(call github,open-power,hostboot,$(HOSTBOOT_P8_VERSION))
 
