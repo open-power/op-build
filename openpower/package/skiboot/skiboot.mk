92f08be483820f905cbf50450af8798acd597124
Reza Arbab <arbab@linux.ibm.com>
2019-02-20 16:42:09 -0600

    skiboot: Add BR2_SKIBOOT_DEVICETREE
    Introduce a buildroot option to build skiboot's external/devicetree
    directory and deliver the binaries as op-build output images.
    
    Signed-off-by: Reza Arbab <arbab@linux.ibm.com>

--- openpower/package/skiboot/skiboot.mk
+++ openpower/package/skiboot/skiboot.mk
@@ -18,6 +18,10 @@ SKIBOOT_LICENSE_FILES = LICENCE
 SKIBOOT_INSTALL_IMAGES = YES
 SKIBOOT_INSTALL_TARGET = NO
 
+ifeq ($(BR2_SKIBOOT_DEVICETREE),y)
+SKIBOOT_DEPENDENCIES += host-dtc
+endif
+
 SKIBOOT_MAKE_OPTS += CC="$(TARGET_CC)" LD="$(TARGET_LD)" \
 		     AS="$(TARGET_AS)" AR="$(TARGET_AR)" NM="$(TARGET_NM)" \
 		     OBJCOPY="$(TARGET_OBJCOPY)" OBJDUMP="$(TARGET_OBJDUMP)" \
@@ -37,10 +41,17 @@ endif
 define SKIBOOT_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) SKIBOOT_VERSION=`cat $(SKIBOOT_VERSION_FILE)` \
 		$(MAKE) $(SKIBOOT_MAKE_OPTS) -C $(@D) all
+
+	$(if $(BR2_SKIBOOT_DEVICETREE), \
+		$(MAKE) -C $(@D)/external/devicetree)
 endef
 
 define SKIBOOT_INSTALL_IMAGES_CMDS
 	$(INSTALL) -D -m 755 $(@D)/skiboot.lid $(BINARIES_DIR)
+
+	$(if $(BR2_SKIBOOT_DEVICETREE), \
+		$(INSTALL) -D -m 644 \
+			$(@D)/external/devicetree/*.dtb $(BINARIES_DIR))
 endef
 
 $(eval $(generic-package))
