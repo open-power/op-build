7b556015e9ea832e4a647385eb8183593308a48f
hostboot <hostboot@us.ibm.com>
2019-04-09 01:12:54 -0500

    op-build update 4-9-2019
    Changes Included for package hostboot, branch master-p8:
    1d4ee5b - Maxim Polyakov - 2019-04-08 - devtree: Add ranges to "ibm,hostboot" node
    
    Changes Included for package hostboot, branch master:
    a9aa7be - Nick Bofferding - 2019-04-08 - Fix page coalesce bug in coalescing end of cache memory range
    ecf2201 - Bill Hoffa - 2019-04-08 - Modify IPMI Timeout Value
    1dc4c34 - Mike Baiocchi - 2019-04-08 - Force UCD Updates on each IPL
    e0fc8dd - Matt Derksen - 2019-04-08 - Add simics exit_cache_contained mode call
    96195e4 - Joachim Fenkes - 2019-04-08 - Fix for workaround for HW480181
    63a8b58 - Joachim Fenkes - 2019-04-08 - p9_tod_setup: Workaround for HW480181
    08d472b - Dan Crowell - 2019-04-08 - Update Explorer FW Command IDs
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/hostboot-p8/hostboot-p8.mk
+++ openpower/package/hostboot-p8/hostboot-p8.mk
@@ -3,7 +3,7 @@
 # hostboot for POWER8
 #
 ################################################################################
-HOSTBOOT_P8_VERSION ?= dc218d94c57b986f5620cc8651f5aae9d08536a6
+HOSTBOOT_P8_VERSION ?= 1d4ee5b3342c9e7aaa6a4b207c782f37c778bf05
 
 HOSTBOOT_P8_SITE ?= $(call github,open-power,hostboot,$(HOSTBOOT_P8_VERSION))
 
