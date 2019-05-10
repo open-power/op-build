bb9c6e892a4079c483d9691dff0b87a8c1fd0a39
hostboot <hostboot@us.ibm.com>
2019-05-10 01:34:49 -0500

    op-build update 5-10-2019
    Changes Included for package hostboot, branch master:
    fe000f8 - Luis Fernandez - 2019-05-09 - Revisit attributes with array types.
    d2da077 - Matthew Raybuck - 2019-05-09 - Targeting updates for EEPROM content type
    c0b91d3 - Glenn Miles - 2019-05-09 - High level support for updating explorer firmware
    bbbd68a - Chen Du - 2019-05-09 - Add page tables to read only partitions
    6aeba39 - Glenn Miles - 2019-05-09 - Increase OCMBFW PNOR partition to 1164kb
    75031a8 - aravnair-in - 2019-05-09 - ZZ-GEN4 and ZZ-2U-GEN4 sysetem binaries need to be built
    7d1cedd - Matthew Raybuck - 2019-05-09 - Add keyword support for OCMB SPD reads
    
    Changes Included for package pnor, branch master:
    af14555 - Dan - 2019-05-09 - Merge pull request #118 from milesg-github/master
    6e3e67f - Glenn Miles - 2019-05-08 - Increase OCMBFW PNOR partition to 1164kb
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/openpower-pnor/openpower-pnor.mk
+++ openpower/package/openpower-pnor/openpower-pnor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_VERSION ?= ac3cec94f52c2d8d7579c43e62f52cd8d7b38f5e
+OPENPOWER_PNOR_VERSION ?= af14555016bde21bc1bee44261faaf1383779129
 OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 
 OPENPOWER_PNOR_LICENSE = Apache-2.0
