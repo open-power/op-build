e8d22e413164639d795e006d736e1096eec57bcf
hostboot <hostboot@us.ibm.com>
2019-07-12 00:46:29 -0500

    op-build update 7-12-2019
    Changes Included for package hostboot, branch master:
    311bb13 - Tsung Yeung - 2019-07-11 - Double nvdimm ops timeout values to accommodate encryption
    264bea8 - Mark Pizzutillo - 2019-07-11 - Add p9a_throttle_sync procedure
    
    Changes Included for package pnor, branch master:
    0346930 - William Hoffa - 2019-07-11 - Merge pull request #122 from wghoffa/master
    36a17c4 - Bill Hoffa - 2019-07-11 - Updates to PNOR Build scripts for OCMBFW image delivery
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/openpower-pnor/openpower-pnor.mk
+++ openpower/package/openpower-pnor/openpower-pnor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_VERSION ?= 43fc2ebbae0f6082520f70b021f958661ee5c994
+OPENPOWER_PNOR_VERSION ?= 03469302c58f65ff57082792cdda159795f6e488
 OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 
 OPENPOWER_PNOR_LICENSE = Apache-2.0
