44763ca1fc2a08fcdc4781191da9ff2928a3311a
Adriana Kobylak <anoo@us.ibm.com>
2019-07-23 09:29:55 -0500

    openpower-pnor-util.mk: Bump to adf91f58da
    Oliver O'Halloran (1):
          Fix generate-tar on ppc64 hosts
    
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- openpower/package/openpower-pnor-util/openpower-pnor-util.mk
+++ openpower/package/openpower-pnor-util/openpower-pnor-util.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOST_OPENPOWER_PNOR_UTIL_VERSION ?= e499446423d6ebe96f74e079de36a8929d47277b
+HOST_OPENPOWER_PNOR_UTIL_VERSION ?= adf91f58dac9f177a061d9b206d853a9db3db70a
 HOST_OPENPOWER_PNOR_UTIL_SITE ?= $(call github,openbmc,openpower-pnor-code-mgmt,$(HOST_OPENPOWER_PNOR_UTIL_VERSION))
 HOST_OPENPOWER_PNOR_UTIL_DEPENDENCIES = host-squashfs host-libflash
 
