dc4a86fa1eeaad72d835a7d8750df7135dea9722
Klaus Heinrich Kiwi <klaus@linux.vnet.ibm.com>
2020-05-11 16:29:14 -0300

    Create virtual packages for OpenPower toolchain
    Create a "toolchain" category, plus "p8-pore-toolchain" and
    "ppe42-toolchain" virtual-packages, with the goal of lowering the
    coupling between these toolchain and target packages.
    
    Signed-off-by: Klaus Heinrich Kiwi <klaus@linux.vnet.ibm.com>

--- openpower/package/ppe42-gcc/ppe42-gcc.mk
+++ openpower/package/ppe42-gcc/ppe42-gcc.mk
@@ -10,6 +10,7 @@ PPE42_GCC_LICENSE = GPLv3+
 
 PPE42_GCC_DEPENDENCIES = ppe42-binutils gmp mpfr mpc
 HOST_PPE42_GCC_DEPENDENCIES = host-ppe42-binutils host-gmp host-mpfr host-mpc
+PPE42_GCC_PROVIDES = ppe42-toolchain
 
 PPE42_GCC_DIR = $(STAGING_DIR)/ppe42-binutils
 PPE42_GCC_BIN = $(STAGING_DIR)/ppe42-binutils/linux
--- openpower/package/sbe/sbe.mk
+++ openpower/package/sbe/sbe.mk
@@ -8,7 +8,7 @@ SBE_VERSION = $(call qstrip,$(BR2_SBE_VERSION))
 SBE_SITE = $(call github,open-power,sbe,$(SBE_VERSION))
 
 SBE_LICENSE = Apache-2.0
-SBE_DEPENDENCIES = host-ppe42-gcc hcode
+SBE_DEPENDENCIES = host-ppe42-toolchain hcode
 
 SBE_INSTALL_IMAGES = YES
 SBE_INSTALL_TARGET = NO
