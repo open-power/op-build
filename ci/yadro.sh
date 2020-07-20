open-power/op-build

commit bbba2e986750d5ec5c7051d132ad805c46eb3738

vesnin: ci: Add support for description in tags
Add support for tags like vesnin-v2.4_pcie_2x8
The part after the first underscore goes into description,
so the resulting version will look like

vesnin-v2.4-pcie_2x8-g123456-unofficial-dirty

This is to enable different feature sets in the same
base version such as 2.4.

Signed-off-by: Alexander Amelkin <a.amelkin@yadro.com>

ci/yadro.sh
@@ -70,8 +70,13 @@ function version_string {
       exit 1
     fi
     local tag="$(${GIT} describe --abbrev=0)"
+    local descr=""
     if [[ ${tag} =~ ${machine}-v[0-9]+\.[0-9]+ ]]; then
       local ver_num="${tag##*-}"
+      ver_num="${ver_num%%_*}"
+      if [[ ${tag} =~ _ ]]; then
+        descr="-${tag#*_}"
+      fi
       local patchlvl="$(${GIT} rev-list --count ${tag}..${branch})"
     else
       # Tag hasn't been set yet, use branch name as major version
@@ -79,7 +84,7 @@ function version_string {
       local patchlvl="$(${GIT} rev-list --count master..${branch})"
     fi
     # Construct version string
-    local version="${machine}-${ver_num}"
+    local version="${machine}-${ver_num}${descr}"
     if [[ ${patchlvl} -ne 0 ]]; then
       version+="-p${patchlvl}"
     fi

commit 4588a865103556560b04be4f7b08b9f70b59b3d0

nicole: ci: Fix "unofficial" status for builds
If a git tag has a postfix (e.g. "_rc1"), the final version string
always gets status "unofficial".
Regression from bbba2e986750d5ec5c7051d132ad805c46eb3738

Signed-off-by: Artem Senichev <a.senichev@yadro.com>

ci/yadro.sh
@@ -89,7 +89,7 @@ function version_string {
       version+="-p${patchlvl}"
     fi
     version+="-g$(${GIT} rev-parse --short=7 HEAD)"
-    if [[ "${ver_num}" == *.0 ]] || [[ ${patchlvl} -ne 0 ]]; then
+    if [[ ${patchlvl} -ne 0 ]]; then
       version+="-unofficial"
     fi
     if ${GIT} describe --dirty | grep -q dirty; then