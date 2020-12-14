open-power/op-build

commit 898245e074f860a743cff05d3f4903aaaa33eabc

skiboot: Change to BR2_PACKAGE_SKIBOOT from BR2_TARGET_SKIBOOT
We think that BR2_TARGET rather than BR2_PACKAGE was originally
used incorrectly as a way to produce a single LID for an FSP build
back sometime in 2014 or before.

Suggested-by: Alastair D'Silva <alastair@d-silva.org>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

openpower/package/openpower-pnor/openpower-pnor.mk
@@ -14,7 +14,7 @@ OPENPOWER_PNOR_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_VERSION))
 OPENPOWER_PNOR_LICENSE = Apache-2.0
 OPENPOWER_PNOR_DEPENDENCIES = hostboot hostboot-binaries $(XML_PACKAGE) skiboot host-openpower-ffs occ capp-ucode
 
-ifeq ($(BR2_TARGET_SKIBOOT_EMBED_PAYLOAD),n)
+ifeq ($(BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD),n)
 
 ifeq ($(BR2_TARGET_ROOTFS_INITRAMFS),y)
 OPENPOWER_PNOR_DEPENDENCIES += linux-rebuild-with-initramfs

openpower/configs/firestone_defconfig
@@ -31,8 +31,8 @@ BR2_OPENPOWER_PNOR_FILENAME="firestone.pnor"
 BR2_OPENPOWER_PNOR_UPDATE_FILENAME="firestone_update.pnor"
 
 # skiboot requirements
-BR2_TARGET_SKIBOOT=y
-BR2_TARGET_SKIBOOT_EMBED_PAYLOAD=n
+BR2_PACKAGE_SKIBOOT=y
+BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD=n
 
 # petitboot requirements
 BR2_ENABLE_LOCALE_PURGE=y

openpower/configs/habanero_defconfig
@@ -31,8 +31,8 @@ BR2_OPENPOWER_PNOR_FILENAME="habanero.pnor"
 BR2_OPENPOWER_PNOR_UPDATE_FILENAME="habanero_update.pnor"
 
 # skiboot requirements
-BR2_TARGET_SKIBOOT=y
-BR2_TARGET_SKIBOOT_EMBED_PAYLOAD=n
+BR2_PACKAGE_SKIBOOT=y
+BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD=n
 
 # petitboot requirements
 BR2_ENABLE_LOCALE_PURGE=y

openpower/configs/palmetto_defconfig
@@ -30,8 +30,8 @@ BR2_OPENPOWER_PNOR_XML_LAYOUT_FILENAME="defaultPnorLayoutSingleSide.xml"
 BR2_OPENPOWER_PNOR_FILENAME="palmetto.pnor"
 
 # skiboot requirements
-BR2_TARGET_SKIBOOT=y
-BR2_TARGET_SKIBOOT_EMBED_PAYLOAD=n
+BR2_PACKAGE_SKIBOOT=y
+BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD=n
 
 # petitboot requirements
 BR2_ENABLE_LOCALE_PURGE=y

openpower/package/skiboot/Config.in
@@ -1,15 +1,15 @@
 menu "Skiboot"
 
-config BR2_TARGET_SKIBOOT
+config BR2_PACKAGE_SKIBOOT
 	bool "skiboot"
 	depends on BR2_powerpc64 || BR2_powerpc64le
         default y if (BR2_OPENPOWER_PLATFORM && BR2_powerpc_power8)
 	help
 	  Build skiboot POWER8 firmware
 
-if BR2_TARGET_SKIBOOT
+if BR2_PACKAGE_SKIBOOT
 
-config BR2_TARGET_SKIBOOT_EMBED_PAYLOAD
+config BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD
 	bool "Embed kernel payload in skiboot image"
 	depends on BR2_LINUX_KERNEL_ZIMAGE_EPAPR
 

commit 3d9cf6a293eb538bb362ed215af8872bd3d8dbb6

Fixes BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD and legal-info
Commit '898245e0 skiboot: Change to BR2_PACKAGE_SKIBOOT from
BR2_TARGET_SKIBOOT' broke the configuration to embed the Kernel
as part of Skiboot.

Additionally, commit '2aff5ba6 Build bootloader kernel seperate from skiboot'
incorrectly assumed that BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD could take the 'n'
value (when it's really just unset when not configured) and introduced a
'linux-rebuild-with-initramfs' dependency that was never exercised (and fails
when building legal-info).

Fixes: 898245e0 skiboot: Change to BR2_PACKAGE_SKIBOOT from BR2_TARGET_SKIBOOT
Fixes: 2aff5ba6 Build bootloader kernel seperate from skiboot

Signed-off-by: Klaus Heinrich Kiwi <klaus@linux.vnet.ibm.com>

openpower/package/openpower-pnor/openpower-pnor.mk
@@ -19,11 +19,9 @@ ifeq ($(BR2_PACKAGE_IMA_CATALOG),y)
 OPENPOWER_PNOR_DEPENDENCIES += ima-catalog
 endif
 
-ifeq ($(BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD),n)
+ifneq ($(BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD),y)
 
 ifeq ($(BR2_TARGET_ROOTFS_INITRAMFS),y)
-OPENPOWER_PNOR_DEPENDENCIES += linux-rebuild-with-initramfs
-else
 OPENPOWER_PNOR_DEPENDENCIES += linux
 endif
 

openpower/package/skiboot/0001-build-Fix-linker-script-for-builtin-Kernel.patch
@@ -1,0 +1,55 @@
+From 6def8ac06ff34b2232de48e5f9bc3a179199a9ad Mon Sep 17 00:00:00 2001
+From: Klaus Heinrich Kiwi <klaus@linux.vnet.ibm.com>
+Date: Thu, 10 Dec 2020 15:44:55 -0500
+Subject: [PATCH] build: Fix linker script for builtin Kernel
+
+Commit '6b08928d - build/lds: place debug sections according to
+defaults' introduced a DEBUG_SECTIONS macro that is effectivelly
+resetting the location pointer back to zero, making the next section
+(builtin_kernel) collide with the earlier sections.
+
+Fix by moving these sections to the very end.
+
+Error message:
+$ make KERNEL=zImage.epapr
+        [CC]  asm/asm-offsets.s
+        [GN]  include/asm-offsets.h
+<...>
+        [LD]  skiboot.tmp.elf
+ld: section .builtin_kernel LMA [0000000000000000,0000000000285d87]
+ overlaps section .head LMA [0000000000000000,0000000000003897]
+ld: section .naca LMA [0000000000004000,000000000000505f] overlaps
+ section .builtin_kernel LMA [0000000000000000,0000000000285d87]
+make: *** [/skiboot/Makefile.main:333: skiboot.tmp.elf] Error 1
+
+Fixes: 6b08928d - build/lds: place debug sections according to defaults
+Signed-off-by: Klaus Heinrich Kiwi <klaus@linux.vnet.ibm.com>
+---
+ skiboot.lds.S | 4 ++--
+ 1 file changed, 2 insertions(+), 2 deletions(-)
+
+diff --git a/skiboot.lds.S b/skiboot.lds.S
+index 5da6f9d8..5a7f9e31 100644
+--- a/skiboot.lds.S
++++ b/skiboot.lds.S
+@@ -233,8 +233,6 @@ SECTIONS
+ 
+ 	ASSERT((HEAP_BASE - SKIBOOT_BASE) >= _end, "Heap collision with image")
+ 
+-	DEBUG_SECTIONS
+-
+ 	/* Optional kernel image */
+        . = ALIGN(PAGE_SIZE);
+        .builtin_kernel : {
+@@ -243,6 +241,8 @@ SECTIONS
+ 		__builtin_kernel_end = .;
+ 	}
+ 
++	DEBUG_SECTIONS
++
+ 	/* Discards */
+ 	/DISCARD/ : {
+ 		*(.note.GNU-stack)
+-- 
+2.27.0
+

openpower/package/skiboot/skiboot.mk
@@ -27,15 +27,9 @@ SKIBOOT_MAKE_OPTS += CC="$(TARGET_CC)" LD="$(TARGET_LD)" \
 		     OBJCOPY="$(TARGET_OBJCOPY)" OBJDUMP="$(TARGET_OBJDUMP)" \
 		     SIZE="$(TARGET_CROSS)size"
 
-ifeq ($(BR2_TARGET_SKIBOOT_EMBED_PAYLOAD),y)
-SKIBOOT_MAKE_OPTS += KERNEL="$(BINARIES_DIR)/$(LINUX_IMAGE_NAME)"
-
-ifeq ($(BR2_TARGET_ROOTFS_INITRAMFS),y)
-SKIBOOT_DEPENDENCIES += linux-rebuild-with-initramfs
-else
+ifeq ($(BR2_PACKAGE_SKIBOOT_EMBED_PAYLOAD),y)
+SKIBOOT_MAKE_OPTS += KERNEL=$(BINARIES_DIR)/$(LINUX_IMAGE_NAME)
 SKIBOOT_DEPENDENCIES += linux
-endif
-
 endif
 
 define SKIBOOT_BUILD_CMDS