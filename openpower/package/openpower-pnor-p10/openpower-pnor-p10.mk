43fcdcfa4cbfea8738b0f300661577446788d606
hostboot <hostboot@us.ibm.com>
2021-09-21 00:55:55 -0500

    op-build update 9-21-2021
    Changes Included for package hcode, branch master-p10:
    1cbe9fe - hostboot - 2021-09-20 - Release tag & head commit information updated for hw092021a.opmst10
    11671cf - Rahul Batra - 2021-09-20 - PGPE: DDS Optimization
    985f908 - Rahul Batra - 2021-09-20 - PGPE: Add support for RVRM bits per quad
    
    Changes Included for package hostboot, branch master-p10:
    ebc600a - Zane Shelley - 2021-09-20 - PRD: power down bad lanes on SMP buses
    df4973b - Chris Cain - 2021-09-20 - HTMGT: Fix APSS traces
    51d0fa0 - Mike Baiocchi - 2021-09-20 - Force Update of Minimum Secure Version to 2 for Early Ship Customers
    0b36d83 - Roland Veloz - 2021-09-20 - MRW: Refactored common code that set the FAPI_I2C_CONTROL_INFO attribute
    192a1a1 - Nick Bofferding - 2021-09-20 - Fix TPM target's HWAS_SUBSCRIPTION_MASK
    8402857 - Zach Clark - 2021-09-20 - Log additional information in PLDM sensor state changed event errors
    cab30c1 - Joe McGill - 2021-09-20 - p10_io_quiesce_lane -- initial release
    
    Changes Included for package pnor, branch master-p10:
    192f672 - Christian Geddes - 2021-09-20 - Merge pull request #147 from crgeddes/add-readonly-tag-hbd
    c762d7e - Christian Geddes - 2021-09-20 - Merge branch 'master-p10' into add-readonly-tag-hbd
    63ead05 - Ilya Smirnov - 2021-09-20 - Merge pull request #146 from crgeddes/remove-ecc
    4e81931 - Ilya Smirnov - 2021-09-20 - Merge pull request #145 from crgeddes/align-staged-binaries
    023d9cd - Christian Geddes - 2021-09-16 - Add readOnly tag to HBD
    355ebe7 - Christian Geddes - 2021-09-16 - Remove <ecc/> tags from all PNOR sections except HBB
    8f3613e - Christian Geddes - 2021-09-16 - Align staged binary files to the nearest 4KB boundary
    
    Changes Included for package hostboot-binaries, branch master:
    1ea0b88 - hostboot - 2021-09-20 - UPDATE executables for the NVIDIA gpu and/or ring files for hw092021a.opmst10
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/openpower-pnor-p10/openpower-pnor-p10.mk
+++ openpower/package/openpower-pnor-p10/openpower-pnor-p10.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_P10_VERSION ?= f8e32b8f41dc5a3657a1c686b4757e21ba84d0fe
+OPENPOWER_PNOR_P10_VERSION ?= 192f6727949d8420196fc5f88ef71f4224393616
 OPENPOWER_PNOR_P10_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_P10_VERSION))
 
 OPENPOWER_PNOR_P10_LICENSE = Apache-2.0
