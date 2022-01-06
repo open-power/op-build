5ab1417255873c929080f902bca6dd0bcda59ec8
hostboot <hostboot@us.ibm.com>
2022-01-06 00:40:33 -0600

    op-build update 1-6-2022
    Changes Included for package hcode, branch master-p10:
    6bf0862 - hostboot - 2022-01-06 - Release tag & head commit information updated for hw010522a.opmst10
    
    Changes Included for package hostboot, branch master-p10:
    a117db3 - Daniel Crowell - 2022-01-05 - Commit info log for scratchreg mismatches on eBMC
    0fd138f - Jayashankar Padath - 2022-01-05 - HDAT: Fix in SLCA index for Ms Area
    348dc28 - Joe McGill - 2022-01-05 - p10_sbe_scratch_regs -- remove PAUC assertion
    21f3aff - Joe McGill - 2022-01-05 - p10_tod_init -- ensure PC timebase facility unmask occurs for OPAL
    f64f276 - Thi Tran - 2022-01-05 - p10_tod_setup - Mask PC TimeBase Facility FIR.
    4545b52 - Matt Derksen - 2022-01-05 - Stop simics after test completion timeout
    62202d7 - Ilya Smirnov - 2022-01-05 - eBMC - Real Time/Date Support: PLDM Requests/Responses
    5318398 - Ilya Smirnov - 2022-01-05 - eBMC - Real Time/Date Support: Date/time Data Structures
    
    Changes Included for package pnor, branch master-p10:
    6d866d3 - Ilya Smirnov - 2022-01-05 - Merge pull request #150 from IlyaSmirnov91/hbd_rw_zero
    93d3140 - Ilya Smirnov - 2022-01-05 - Merge pull request #148 from IlyaSmirnov91/master-p10
    a5677cd - Ilya Smirnov - 2021-12-03 - Remove targeting_RW_binary_source parameter from update_image.pl
    25742b5 - Ilya Smirnov - 2021-11-09 - Mark HBD_RW as Preserved
    
    Changes Included for package hostboot-binaries, branch master:
    c4531d7 - hostboot - 2022-01-06 - UPDATE executables for the NVIDIA gpu and/or ring files for hw010522a.opmst10
    2187434 - hostboot - 2022-01-04 - UPDATE executables for the NVIDIA gpu and/or ring files for hw010322a.opmst
    c7ec58b - hostboot - 2022-01-03 - UPDATE executables for the NVIDIA gpu and/or ring files for hw010322a.opmst10
    
    Signed-off-by: hostboot <hostboot@us.ibm.com>

--- openpower/package/openpower-pnor-p10/openpower-pnor-p10.mk
+++ openpower/package/openpower-pnor-p10/openpower-pnor-p10.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENPOWER_PNOR_P10_VERSION ?= d89f8a2e6fc3f1fe593a102651e10c5901dfb42b
+OPENPOWER_PNOR_P10_VERSION ?= 6d866d38266f683fa1f09df2b40779c6cb02bf0d
 OPENPOWER_PNOR_P10_SITE ?= $(call github,open-power,pnor,$(OPENPOWER_PNOR_P10_VERSION))
 
 OPENPOWER_PNOR_P10_LICENSE = Apache-2.0
