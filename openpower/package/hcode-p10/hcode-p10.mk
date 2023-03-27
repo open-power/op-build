5ac33667a5d824864b4525b7a6b867f94cf2e4d0
premjha2@in.ibm.com <premjha2@in.ibm.com>
2023-03-27 01:38:50 -0500

    p10_hcode: Partial support for parsing of traces in PM elog
    Commit copies PM trexStringFiles from build directory to
    PNOR stage directory as LID.
    
    Signed-off-by: premjha2@in.ibm.com <premjha2@in.ibm.com>

--- openpower/package/hcode-p10/hcode-p10.mk
+++ openpower/package/hcode-p10/hcode-p10.mk
@@ -16,6 +16,9 @@ HCODE_P10_DEPENDENCIES = host-binutils host-ppe42-gcc hostboot-binaries
 HW_IMAGE_BIN_PATH = output/images/hw_image
 HW_IMAGE_BIN_NAME = p10.hw_image.bin
 HCODE_IMAGE_BIN_NAME = p10.ref_image.bin
+QME20_TREXSTRING_PATH=hcode/qme_p10dd20/
+XGPE20_TREXSTRING_PATH=hcode/xgpe_p10dd20/
+PGPE20_TREXSTRING_PATH=hcode/pgpe_p10dd20/
 
 CROSS_COMPILER_PATH=$(PPE42_GCC_BIN)
 PPE_TOOL_PATH ?= $(CROSS_COMPILER_PATH)
@@ -39,13 +42,21 @@ HCODE_P10_ENV_VARS= CONFIG_FILE=$(BR2_EXTERNAL_OP_BUILD_PATH)/configs/hcode/$(BR
 	CONFIG_IONV_FILE_LOCATION=$(STAGING_DIR)/hostboot_binaries/$(BINARY_IONV_FILENAME) \
 	CONFIG_INCLUDE_IONV=$(BR2_HCODE_INCLUDE_IONV) OPENPOWER_BUILD=1
 
+
 define HCODE_P10_INSTALL_IMAGES_CMDS
 	mkdir -p $(STAGING_DIR)/hcode
+    mkdir -p $(STAGING_DIR)/$(QME20_TREXSTRING_PATH)
+	mkdir -p $(STAGING_DIR)/$(XGPE20_TREXSTRING_PATH)
+	mkdir -p $(STAGING_DIR)/$(PGPE20_TREXSTRING_PATH)
+	$(INSTALL) $(@D)/output/images/qme_p10dd20/trexStringFile $(STAGING_DIR)/$(QME20_TREXSTRING_PATH)
+	$(INSTALL) $(@D)/output/images/xgpe_p10dd20/trexStringFile $(STAGING_DIR)/$(XGPE20_TREXSTRING_PATH)
+	$(INSTALL) $(@D)/output/images/pgpe_p10dd20/trexStringFile $(STAGING_DIR)/$(PGPE20_TREXSTRING_PATH)
 	$(INSTALL) $(@D)/$(HW_IMAGE_BIN_PATH)/$(HW_IMAGE_BIN_NAME) $(STAGING_DIR)/hcode/$(HCODE_IMAGE_BIN_NAME)
 endef
 
 define HCODE_P10_BUILD_CMDS
 	$(HCODE_P10_ENV_VARS) bash -c 'cd $(@D) && source ./env.bash && $(MAKE) '
 endef
+
 
 $(eval $(generic-package))
