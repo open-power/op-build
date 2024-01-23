0e9e3c78d824d00342b06b61f64db64f846e4b05
Daniel Crowell <dcrowell@us.ibm.com>
2024-01-23 15:50:59 -0600

    Enable Odyssey by default
    All P10 systems should now support Odyssey.  Note that this
    does require some internal IBM access (for a couple more months).
    
    Modify odyssey sbe build command.
    
    Signed-off-by: Daniel Crowell <dcrowell@us.ibm.com>

--- openpower/package/sbe-odyssey/sbe-odyssey.mk
+++ openpower/package/sbe-odyssey/sbe-odyssey.mk
@@ -1,38 +1,90 @@
+\begindata{text,134763032}
+\textdsversion{12}
+\define{function
+}
+\define{keyword
+}
+
 
 SBE_ODYSSEY_VERSION = $(call qstrip,$(BR2_SBE_ODYSSEY_VERSION))
+
 SBE_ODYSSEY_SITE ?= git@github.ibm.com:open-power/sbe-common.git
+
 SBE_ODYSSEY_SITE_METHOD = git
 
+
 SBE_ODYSSEY_LICENSE = Apache-2.0
-SBE_ODYSSEY_DEPENDENCIES = host-ppe42-gcc host-meson host-python3 op-image-tools
+
+SBE_ODYSSEY_DEPENDENCIES = host-ppe42-gcc host-meson host-python3 
+op-image-tools
+
 
 SBE_ODYSSEY_INSTALL_IMAGES = YES
+
 SBE_ODYSSEY_INSTALL_TARGET = NO
+
 
 CMD_VARS = LD_LIBRARY_PATH=$(HOST_DIR)/lib PATH="$(HOST_DIR)/bin:$$PATH"
 
-OP_IMAGE_TOOLS_PATH = $(BUILD_DIR)/op-image-tools-$(BR2_OP_IMAGE_TOOLS_VERSION)
+
+OP_IMAGE_TOOLS_PATH = 
+$(BUILD_DIR)/op-image-tools-$(BR2_OP_IMAGE_TOOLS_VERSION)
+
 
 define SBE_ODYSSEY_BUILD_CMDS
+
 	$(CMD_VARS) bash -c "python3 -m ensurepip"
-	echo 'export LD_LIBRARY_PATH=$(HOST_DIR)/lib:$$LD_LIBRARY_PATH' >> $(@D)/customrc
+
+	echo 'export LD_LIBRARY_PATH=$(HOST_DIR)/lib:$$LD_LIBRARY_PATH' >> 
+$(@D)/customrc
+
 	echo 'export PPETOOLS=$(PPE42_GCC_BIN)' >> $(@D)/customrc
+
 	echo 'export USER_BIN_DIR=$(@D)/.venv/bin' >> $(@D)/customrc
+
 	echo 'export PYTHONUSERBASE="$(HOST_DIR)"' >> $(@D)/customrc
-	echo 'export PYTHON_USER_PACKAGE_PATH="$(wildcard $(HOST_DIR)/lib/python*/site-packages)"' >> $(@D)/customrc
+
+	echo 'export PYTHON_USER_PACKAGE_PATH="$(wildcard 
+$(HOST_DIR)/lib/python*/site-packages)"' >> $(@D)/customrc
+
 	echo '. .venv/bin/activate' >> $(@D)/customrc
+
 	sed -i 's/--user//g' $(@D)/public/src/tools/utils/sbe/sbe-workon-utils
+
 	$(HOST_DIR)/bin/python3 -m venv $(@D)/.venv --prompt="SBE"
-	cd $(@D) && set -m && ./sbe workon odyssey pnor <<< '. customrc && unset SIGNING_BASE_DIR && ./sbe build && exit' > /dev/null 2>&1
-	$(OP_IMAGE_TOOLS_PATH)/imageBuild/imageBuild.py --sbe $(BUILD_DIR)/sbe-odyssey-$(BR2_SBE_ODYSSEY_VERSION)/ --ovrd $(BUILD_DIR)/hostboot-binaries-$(BR2_HOSTBOOT_BINARIES_VERSION)/ -o $(STAGING_DIR)/ody-pak-files --build_workdir $(STAGING_DIR)/ody-pak-files.work $(OP_IMAGE_TOOLS_PATH)/imageBuild/configs/odyssey/dd1/ody_pnor_dd1_image_config
+
+	cd $(@D) && ./internal/src/test/framework/build-script odyssey pnor 
+--skip-tests > /dev/null 2>&1
+
+	$(OP_IMAGE_TOOLS_PATH)/imageBuild/imageBuild.py --sbe 
+$(BUILD_DIR)/sbe-odyssey-$(BR2_SBE_ODYSSEY_VERSION)/ --ovrd 
+$(BUILD_DIR)/hostboot-binaries-$(BR2_HOSTBOOT_BINARIES_VERSION)/ -o 
+$(STAGING_DIR)/ody-pak-files --build_workdir 
+$(STAGING_DIR)/ody-pak-files.work 
+$(OP_IMAGE_TOOLS_PATH)/imageBuild/configs/odyssey/dd1/ody_pnor_dd1_image_con\
+fig
+
 endef
+
 
 define SBE_ODYSSEY_INSTALL_IMAGES_CMDS
+
 	mkdir -p $(STAGING_DIR)/ody_binaries
+
 	mkdir -p $(STAGING_DIR)/poz_debug_tools
-        cp $(STAGING_DIR)/ody-pak-files/gen/final/boot.pak $(STAGING_DIR)/ody_binaries/
-        cp $(STAGING_DIR)/ody-pak-files/gen/final/rt.pak $(STAGING_DIR)/ody_binaries/
-        cp $(@D)/images/odyssey/odyssey_sbe_debug_DD1.tar.gz $(STAGING_DIR)/poz_debug_tools/
+
+        cp $(STAGING_DIR)/ody-pak-files/gen/final/boot.pak 
+$(STAGING_DIR)/ody_binaries/
+
+        cp $(STAGING_DIR)/ody-pak-files/gen/final/rt.pak 
+$(STAGING_DIR)/ody_binaries/
+
+        cp $(@D)/images/odyssey/odyssey_sbe_debug_DD1.tar.gz 
+$(STAGING_DIR)/poz_debug_tools/
+
 endef
 
+
 $(eval $(generic-package))
+
+\enddata{text,134763032}
