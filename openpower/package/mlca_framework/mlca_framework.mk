51bf311ddd0ad4e1569a235aba3ba91691f6d88c
Chris Engel <cjengel@us.ibm.com>
2024-05-30 07:25:05 -0500

    Add v2 signing support for Odyssey
    Signed-off-by: Chris Engel <cjengel@us.ibm.com>

---
+++ openpower/package/mlca_framework/mlca_framework.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+#  mlca_framework
+#
+################################################################################
+
+MLCA_FRAMEWORK_SITE ?= $(call github,IBM,mlca,$(MLCA_FRAMEWORK_VERSION))
+
+MLCA_FRAMEWORK_LICENSE = Apache-2.0
+MLCA_FRAMEWORK_LICENSE_FILES = LICENSE
+MLCA_FRAMEWORK_VERSION ?= 5276fac56d1caf1ad9e249df58547cc5c029fbd8
+
+HOST_MLCA_FRAMEWORK_DEPENDENCIES =
+
+define HOST_MLCA_FRAMEWORK_BUILD_CMDS
+	cd $(@D) && mkdir -p build && cd build && \
+	cmake .. && ${MAKE}
+endef
+
+define HOST_MLCA_FRAMEWORK_INSTALL_CMDS
+	cd $(@D) && \
+    $(INSTALL) -m 0755 $(@D)/build/libmlca_shared.so $(HOST_DIR)/usr/lib/ && \
+	$(INSTALL) -m 0644 $(@D)/include/* $(HOST_DIR)/usr/include/
+	$(INSTALL) -m 0644 $(@D)/qsc/crystals/crystals-oids.h $(HOST_DIR)/usr/include/
+	$(INSTALL) -m 0644 $(@D)/qsc/crystals/pqalgs.h $(HOST_DIR)/usr/include/
+endef
+
+$(eval $(host-generic-package))
---
+++ openpower/package/sb-signing-utils/v2_keys/gen_boot_keys_hash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ ../../hashkeys -V 2 -a boot_hw_key_a.pub -d boot_hw_key_d.pub --pretty --outfile boot_hw_keys_hash.md
