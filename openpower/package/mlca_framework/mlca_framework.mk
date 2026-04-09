369f9b55d85af10d739615f44b40aa69db6b7dbd
Reza Arbab <arbab@linux.ibm.com>
2026-04-09 17:53:05 -0500

    package/mlca_framework: Use host-cmake
    This package is not using cmake from the buildroot environment but
    instead attempting to use /usr/bin/cmake from the builder machine,
    causing a build break on systems that do not have it installed.
    
    Fix up the makefile to properly use host-cmake-package.
    
    Signed-off-by: Reza Arbab <arbab@linux.ibm.com>

--- openpower/package/mlca_framework/mlca_framework.mk
+++ openpower/package/mlca_framework/mlca_framework.mk
@@ -10,19 +10,11 @@ MLCA_FRAMEWORK_LICENSE = Apache-2.0
 MLCA_FRAMEWORK_LICENSE_FILES = LICENSE
 MLCA_FRAMEWORK_VERSION ?= ef6324abb7684464829fe2baa2ace9643b16d85b
 
-HOST_MLCA_FRAMEWORK_DEPENDENCIES =
-
-define HOST_MLCA_FRAMEWORK_BUILD_CMDS
-	cd $(@D) && mkdir -p build && cd build && \
-	cmake .. && ${MAKE}
-endef
-
 define HOST_MLCA_FRAMEWORK_INSTALL_CMDS
-	cd $(@D) && \
-    $(INSTALL) -m 0755 $(@D)/build/libmlca_shared.so $(HOST_DIR)/usr/lib/ && \
+	$(INSTALL) -m 0755 $(@D)/libmlca_shared.so $(HOST_DIR)/usr/lib/
 	$(INSTALL) -m 0644 $(@D)/include/* $(HOST_DIR)/usr/include/
 	$(INSTALL) -m 0644 $(@D)/qsc/crystals/crystals-oids.h $(HOST_DIR)/usr/include/
 	$(INSTALL) -m 0644 $(@D)/qsc/crystals/pqalgs.h $(HOST_DIR)/usr/include/
 endef
 
-$(eval $(host-generic-package))
+$(eval $(host-cmake-package))
